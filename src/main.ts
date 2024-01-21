c78c8c97e77e5a6efb24ba52e708957695dcba8f
阮志华 <2529720581@qq.com>
2024-01-21 14:43:47 +0800

    引入animate.css

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "./assets/scss/style.scss";
 import "./assets/scss/highlight-vs.scss";
 import './assets/scss/loading-animate.scss'
 import router from "./router/index";
+import 'animate.css'
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
