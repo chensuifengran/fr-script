90ba9684b4db565a13a7cece715bfee33994411c
阮志华 <2529720581@qq.com>
2024-01-27 15:35:23 +0800

    修复快速参数替换参数不正确的问题

--- src/invokes/moveTo/auxiliary.ts
+++ src/invokes/moveTo/auxiliary.ts
@@ -1,13 +1,14 @@
 const { getInvokeApiMethods } = useInvokeApiMethodsRegister();
 export const auxiliary = <AuxiliaryType>{
-  //参数回填方法
+  //参数回填方法，快速填写参数弹窗打开时调用，用于弹窗内参数值的回填
   parameterBackfill: async (...args: string[]) => {
     let params = await AutoTipUtils.paramsProcess(args);
+    //当前函数的信息
     const selfModule = getInvokeApiMethods().find((i) => i.name === "moveTo");
-    let rangeStr = '';
-    if(params.length > 2){
-      for(let i = 2; i < params.length; i++){
-        rangeStr += ','+params[i];
+    let rangeStr = "";
+    if (params.length > 2) {
+      for (let i = 2; i < params.length; i++) {
+        rangeStr += "," + params[i];
       }
     }
     rangeStr = rangeStr.slice(1);
@@ -20,16 +21,16 @@ export const auxiliary = <AuxiliaryType>{
           i.value = +(params[1] || "");
           break;
         case 2:
-          let value = [0,0];
-          if(rangeStr.length){
+          let value = [0, 0];
+          if (rangeStr.length) {
             value = JSON.parse(rangeStr)[0];
           }
           i.value = value;
           break;
         case 3:
-          let value1 = [0,0];
-          if(rangeStr.length){
-            value1 = JSON.parse(rangeStr)[1] || [0,0];
+          let value1 = [0, 0];
+          if (rangeStr.length) {
+            value1 = JSON.parse(rangeStr)[1] || [0, 0];
           }
           i.value = value1;
           break;
@@ -38,15 +39,24 @@ export const auxiliary = <AuxiliaryType>{
       }
     });
   },
-  //参数处理方法
+  //参数处理方法,快速填写参数弹窗点击确定后调用，replaceCurFnArgs方法用于替换编辑器中当前函数的参数
   parameterReplace: (options: {
     x: number;
     y: number;
-    randomRange: [[number, number], [number, number]];
+    xRandomRange: [number, number];
+    yRandomRange: [number, number];
     replaceCurFnArgs: (targetArgs: string) => void;
   }) => {
-    options.replaceCurFnArgs(
-      `"${options.x}", "${options.y}", "${JSON.stringify(options.randomRange)}"`
-    );
+    if (
+      options.xRandomRange[0] === 0 &&
+      options.xRandomRange[1] === 0 &&
+      options.yRandomRange[0] === 0 &&
+      options.yRandomRange[1] === 0
+    ) {
+      options.replaceCurFnArgs(`${options.x}, ${options.y}`);
+    } else
+      options.replaceCurFnArgs(
+        `${options.x}, ${options.y}, [[${options.xRandomRange[0]}, ${options.xRandomRange[1]}], [${options.yRandomRange[0]}, ${options.yRandomRange[1]}]]`
+      );
   },
 };
