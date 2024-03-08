79a3983de6fdc32586b769acf79f9a8d07b7cd6c
阮志华 <2529720581@qq.com>
2024-03-08 22:53:10 +0800

    createWindow方法传入参数时会和默认配置项进行合并、其它细节优化

--- src/hooks/useWebviewWindow.ts
+++ src/hooks/useWebviewWindow.ts
@@ -1,9 +1,4 @@
 import { WebviewWindow, WindowOptions } from "@tauri-apps/api/window";
-import { GlobalWindowInfo } from "../types/webviewWindow";
-
-const globalWindowInfo = ref<GlobalWindowInfo>({
-  windows: [],
-});
 
 export const useWebviewWindow = () => {
   const webview = ref<WebviewWindow | null>(null);
@@ -12,40 +7,44 @@ export const useWebviewWindow = () => {
     routePath: string,
     options?: WindowOptions
   ) => {
-    const globalTarget = globalWindowInfo.value.windows.find((item) => {
-      return item.label === label;
-    });
-    if (!globalTarget) {
-      if (!options) {
-        options = {
-          fullscreen:false,
-          resizable: true,
-          title: label,
-          decorations: false,
-          transparent: true,
-          width: 800,
-          height: 600,
-          minWidth: 800,
-        };
+    try {
+      const globalTarget = WebviewWindow.getByLabel(label);
+      const defaultOps = {
+        fullscreen: false,
+        resizable: true,
+        title: label,
+        decorations: false,
+        transparent: true,
+        width: 800,
+        height: 600,
+        minWidth: 800,
+      };
+      if (!globalTarget) {
+        if (!options) {
+          options = defaultOps;
+        } else {
+          options = {
+            ...defaultOps,
+            ...options,
+          };
+        }
+        const urlPath = `./index.html/#${routePath}`;
+        const url = new URL(urlPath, import.meta.url).href;
+        const window = new WebviewWindow(label, {
+          ...options,
+          url,
+        });
+        webview.value = window;
+
+        return window;
+      } else {
+        return globalTarget;
       }
-      const urlPath = `./index.html/#${routePath}`;
-      const url = new URL(urlPath, import.meta.url).href;
-      const window = new WebviewWindow(label, {
-        ...options,
-        url,
-      });
-      webview.value = window;
-      globalWindowInfo.value.windows.push({
-        label,
-        window,
-      });
-      return window;
-    } else {
-      return globalTarget.window;
+    } catch (error) {
+      console.error("useWebviewWindow error", error);
     }
   };
   return {
-    globalWindowInfo,
     webview,
     createWindow,
   };
