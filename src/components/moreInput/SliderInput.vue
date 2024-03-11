a7e84f5f3beb8ddcdbb3cbca94f7287073bc0d6d
阮志华 <2529720581@qq.com>
2024-03-11 19:18:33 +0800

    封装el-slider组件，重写样式

---
+++ src/components/moreInput/SliderInput.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="fr-slider">
+    <el-text v-if="props.label" class="label">{{ props.label }}</el-text>
+    <div class="data-area">
+      <el-text class="mgr-10">{{ value }}</el-text>
+      <el-slider
+        :style="{
+          width: props.label ? props.width + 'px' : '100%',
+        }"
+        class="slider"
+        v-model="value"
+        :min="props.min"
+        :max="props.max"
+        :step="props.step"
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const value = defineModel<number>({
+  default: 0,
+});
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  min: {
+    type: Number,
+    default: 0,
+  },
+  max: {
+    type: Number,
+    default: 100,
+  },
+  step: {
+    type: Number,
+    default: 1,
+  },
+  width: {
+    type: Number,
+    default: 100,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.mgr-10 {
+  margin-right: 10px;
+}
+.fr-slider {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 5px;
+  .label {
+    flex: 1;
+    margin-right: 100px;
+  }
+  .data-area {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+}
+</style>
+<style lang="scss">
+.fr-slider {
+  .el-slider {
+    position: relative;
+    height: 16px;
+    .el-slider__runway {
+      border-radius: 8px;
+      overflow: hidden;
+      height: 100%;
+      cursor: e-resize;
+      .el-slider__bar {
+        height: 100%;
+      }
+      .el-slider__button-wrapper {
+        // display: none;
+        height: 100%;
+        top: 0;
+        visibility: hidden;
+      }
+    }
+  }
+}
+</style>
