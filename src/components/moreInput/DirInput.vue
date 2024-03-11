14e7999c4a7b947603db73068fd936d21f39d397
阮志华 <2529720581@qq.com>
2024-03-11 15:43:54 +0800

    defineModel简化封装

--- src/components/moreInput/DirInput.vue
+++ src/components/moreInput/DirInput.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <el-input
-      spellcheck="false"
-      v-model="localValue"
-      @input="$emit('update:modelValue', $event)"
-    >
+    <el-input spellcheck="false" v-model="value">
       <template #prepend v-if="label !== ''"
-        ><span style="color: red" v-if="localValue.trim().length && !pathExits">{{
-          label
-        }}</span
-        ><span v-else>{{ label }}</span></template
+        ><span
+          :style="{
+            color: pathExits ? undefined : 'red',
+          }"
+          >{{ label }}</span
+        ></template
       >
       <template #append>
         <el-button @click="selectFilePath">选择路径</el-button>
       </template>
     </el-input>
-    <div v-show="localValue.trim().length && !pathExits" class="tip">
+    <div v-show="!pathExits" class="tip">
       <el-icon color="red"><IEpCircleCloseFilled /></el-icon
       ><el-tag type="danger"
         >该路径无效，请检查路径填写是否有误，请检查路径填写是否有误</el-tag
@@ -26,12 +24,11 @@
 
 <script setup lang="ts">
 import { exists } from "@tauri-apps/api/fs";
-
+const value = defineModel<string>({
+  default: "",
+});
+const pathExits = ref(true);
 const props = defineProps({
-  modelValue: {
-    type: String,
-    default: "",
-  },
   label: {
     type: String,
     default: "",
@@ -44,39 +41,15 @@ const props = defineProps({
     type: String,
     default: "",
   },
-  mountedValue: {
-    type: String,
-    default: "",
-  },
-  parentVisible: {
+  verify: {
     type: Boolean,
-    default: true,
+    default: false,
   },
 });
-const localValue = ref(props.modelValue);
-const pathExits = ref(false);
-// 监听visible变量的变化
-watch(
-  () => props.parentVisible,
-  async (newValue) => {
-    if (newValue) {
-      // visible为true时，执行检测方法
-      pathExits.value = await exists(localValue.value);
-    }
-  },
-  { deep: true } // 设置深度监听
-);
-
-watchEffect(async () => {
-  if (props.modelValue !== localValue.value) {
-    localValue.value = props.modelValue;
-    pathExits.value = await exists(localValue.value);
-  }
+watch(value, async () => {
+  if (!props.verify) return;
+  pathExits.value = await exists(value.value);
 });
-
-const emit = defineEmits<{
-  (e: "update:modelValue", value: string): void;
-}>();
 const selectFilePath = async () => {
   let filePath = (await fsUtils.selectFile()) as string | undefined;
   if (props.suffix && props.suffix.length > 0) {
@@ -85,25 +58,13 @@ const selectFilePath = async () => {
   if (props.prefix && props.prefix.length > 0) {
     filePath = props.prefix + filePath;
   }
-  emit("update:modelValue", filePath || localValue.value || "");
-  localValue.value = filePath || "";
+  value.value = filePath || "";
 };
-watch(localValue, async () => {
-  pathExits.value = await exists(localValue.value);
-});
-onMounted(async () => {
-  if (props.mountedValue && props.mountedValue.length > 0) {
-    localValue.value = props.mountedValue;
-    emit("update:modelValue", props.mountedValue);
-  }
-  pathExits.value = await exists(localValue.value);
-});
 </script>
 
 <style lang="scss" scoped>
 .tip {
   display: flex;
-  // justify-content: center;
   align-items: center;
 }
 </style>
