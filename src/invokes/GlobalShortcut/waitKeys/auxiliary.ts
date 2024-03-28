7ec5dd25fa1031ef07dad48fdf9bd0e825567f5e
尘随风染 <2529720581@qq.com>
2024-03-28 19:52:38 +0800

    修复快速填写参数弹窗打开时参数读取错误

--- src/invokes/GlobalShortcut/waitKeys/auxiliary.ts
+++ src/invokes/GlobalShortcut/waitKeys/auxiliary.ts
@@ -1,5 +1,3 @@
-
-
 export const auxiliary = <AuxiliaryType>{
   //参数回填方法
   parameterBackfill: async (...args: string[]) => {
@@ -8,9 +6,13 @@ export const auxiliary = <AuxiliaryType>{
       (i) => i.name === "waitKeys" && i.scope === "GlobalShortcut"
     );
     const dialog = selfModule!.testModule!.dialog;
-    dialog.args!.forEach((i, index) => {
-      i.value = params[index] || [];
-    });
+    if (dialog.args && dialog.args[0]) {
+      dialog.args[0].value = params.map((p) => {
+        //替换掉p的[和]
+        p = p.replace(/[\[\]]/g, "");
+        return p;
+      });
+    };
   },
   //参数处理方法
   parameterReplace: (options: {
@@ -18,9 +20,7 @@ export const auxiliary = <AuxiliaryType>{
     replaceCurFnArgs: (targetArgs: string) => void;
   }) => {
     options.replaceCurFnArgs(
-      `[
-        ${options.keys.map((key) => `"${key}"`).join(", ")}
-      ]`
+      `[${options.keys.map((key) => `"${key}"`).join(", ")}]`
     );
   },
 };
