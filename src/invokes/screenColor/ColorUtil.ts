19df3507928da29c68440543be1b96eabcf9d3ac
尘随风染 <2529720581@qq.com>
2024-04-11 16:59:43 +0800

    优化等待颜色出现方法，新增等待颜色取消方法

--- src/invokes/screenColor/ColorUtil.ts
+++ src/invokes/screenColor/ColorUtil.ts
@@ -26,13 +26,19 @@ export class ColorUtil {
     ];
     return colorArr;
   };
-  is = (color: string) => {
+  is = (
+    color: string,
+    allowOffsetRange: [number, number, number] = [0, 0, 0]
+  ) => {
     const colorArr = this.parseColorStr(color);
     if (!colorArr) {
       return false;
     }
     if (colorArr.length === 3) {
-      return this.rgb.every((v, i) => v === colorArr[i]);
+      return this.rgb.every((v, i) => {
+        const offset = allowOffsetRange[i];
+        return v >= colorArr[i] - offset && v <= colorArr[i] + offset;
+      });
     } else {
       console.error("color格式错误");
       return false;
@@ -41,15 +47,43 @@ export class ColorUtil {
   waitColor = async (
     color: string,
     sleepMs: number = 1000,
-    maxWaitCount: number = 10
+    maxWaitCount: number = 10,
+    allowOffsetRange: [number, number, number] = [0, 0, 0],
+    mismatchCallback?: (curColor: string, expectedColor: string) => void
   ) => {
-    if (this.is(color)) {
+    if (this.is(color, allowOffsetRange)) {
+      return true;
+    } else {
+      mismatchCallback && mismatchCallback(this.getRgbValue(), color);
+    }
+    let result = false;
+    while (maxWaitCount--) {
+      const reCallRes = await this.reCall();
+      if (reCallRes && reCallRes.is(color, allowOffsetRange)) {
+        result = true;
+        break;
+      } else {
+        reCallRes &&
+          mismatchCallback &&
+          mismatchCallback(reCallRes.getRgbValue(), color);
+      }
+      await timeUtil.sleep(sleepMs);
+    }
+    return result;
+  };
+  waitNotColor = async (
+    color: string,
+    sleepMs: number = 1000,
+    maxWaitCount: number = 10,
+    allowOffsetRange: [number, number, number] = [0, 0, 0]
+  ) => {
+    if (!this.is(color, allowOffsetRange)) {
       return true;
     }
     let result = false;
     while (maxWaitCount--) {
       const reCallRes = await this.reCall();
-      if (reCallRes && reCallRes.is(color)) {
+      if (reCallRes && !reCallRes.is(color, allowOffsetRange)) {
         result = true;
         break;
       }
--- src/invokes/screenColor/declaration.ts
+++ src/invokes/screenColor/declaration.ts
@@ -1,21 +1,60 @@
 export const declaration = `
 declare class ColorUtil {
-    public rgb: [number, number, number];
-    private reCall: () => Promise<ColorUtil | undefined>;
-    constructor(
-      rgb: [number, number, number],
-      reCall: () => Promise<ColorUtil | undefined>
-    );
-    getHex: () => string;
-    getRgb: () => string;
-    getRgbValue: () => string;
-    parseColorStr: (color: string) => [number, number, number] | undefined;
-    is: (color: string) => boolean;
-    waitColor: (
-      color: string,
-      sleepMs?: number,
-      maxWaitCount?: number
-    ) => Promise<boolean>;
+  public rgb: [number, number, number];
+  private reCall: () => Promise<ColorUtil | undefined>;
+  constructor(
+    rgb: [number, number, number],
+    reCall: () => Promise<ColorUtil | undefined>
+  );
+  getHex: () => string;
+  /**
+   * 获取rgb字符串
+   * 如：rgb(255,255,255)
+  */
+  getRgb: () => string;
+  /**
+   * 获取rgb值字符串（仅获取值）
+   * 如：255,255,255
+  */
+  getRgbValue: () => string;
+  parseColorStr: (color: string) => [number, number, number] | undefined;
+  /**
+   * 判断颜色是否匹配
+   * @param color 颜色字符串
+   * @param allowOffsetRange 允许的偏移范围
+   * @returns 是否匹配
+  */
+  is: (color: string, allowOffsetRange?:[number, number, number]) => boolean;
+  /**
+   * 等待该位置出现预期颜色
+   * @param color 预期颜色字符串
+   * @param sleepMs 检测间隔，默认1000ms
+   * @param maxWaitCount 最大检测次数,默认10次
+   * @param allowOffsetRange rgb值允许的偏移范围，默认[0,0,0]
+   * @param mismatchCallback 颜色不匹配时的回调
+   * @returns 是否匹配
+  */
+  waitColor: (
+    color: string,
+    sleepMs?: number,
+    maxWaitCount?: number,
+    allowOffsetRange?:[number, number, number],
+    mismatchCallback?: (curColor: string, expectedColor: string) => void
+  ) => Promise<boolean>;
+  /**
+   * 等待该位置颜色消失
+   * @param color 颜色字符串
+   * @param sleepMs 检测间隔，默认1000ms
+   * @param maxWaitCount 最大检测次数,默认10次
+   * @param allowOffsetRange rgb值允许的偏移范围，默认[0,0,0]
+   * @returns 是否消失
+  */
+  waitNotColor: (
+    color: string,
+    sleepMs?: number,
+    maxWaitCount?: number,
+    allowOffsetRange?:[number, number, number]
+  ) => Promise<boolean>;
 }
 declare function screenColor(
     x?:number,
