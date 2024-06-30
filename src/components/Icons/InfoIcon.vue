e7b6be590050cccfeaf8faace5d24395bc9f877b
尘随风染 <2529720581@qq.com>
2024-06-30 10:26:58 +0800

    更换了使用的图标

--- src/components/Icons/InfoIcon.vue
+++ src/components/Icons/InfoIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-icon :size="size"><span i-mdi-information-slab-circle></span></el-icon>
+  <el-icon :size="size"><span i-mdi-information-slab-circle-outline></span></el-icon>
 </template>
 <script lang="ts" setup>
 defineProps({
