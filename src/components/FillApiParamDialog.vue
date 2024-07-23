9d051256a94f886267d710ec021f78d1467cddf7
尘随风染 <2529720581@qq.com>
2024-07-23 16:14:08 +0800

    refactor: 简化代码

--- src/components/FillApiParamDialog.vue
+++ src/components/FillApiParamDialog.vue
@@ -1,22 +1,11 @@
 <template>
-  <GeneralDialog
-    v-model="dynamicDialog.show"
-    :title="dynamicDialog.title"
-    :content="dynamicDialog.content"
-    :callback="dynamicDialog.callback"
-    :isTestModule="true"
-  >
+  <GeneralDialog v-model="dynamicDialog.show" :title="dynamicDialog.title" :content="dynamicDialog.content"
+    :callback="dynamicDialog.callback" :isTestModule="true">
     <template #element>
       <div v-for="m in invokeApiTestModules">
-        <DynamicsInput
-          v-for="a in m?.dialog?.args"
-          :key="m!.dialog?.targetMethodName + a.name"
-          :name="m!.dialog?.targetMethodName || ''"
-          :argName="a.name"
-          :value="a.value"
-          :only-test="a.onlyTest ? true : false"
-          :no-test="a.noTest ? true : false"
-        />
+        <DynamicsInput v-for="a in m?.dialog?.args" :key="m!.dialog?.targetMethodName + a.name"
+          :name="m!.dialog?.targetMethodName || ''" :argName="a.name" :value="a.value"
+          :only-test="a.onlyTest ? true : false" :no-test="a.noTest ? true : false" />
       </div>
     </template>
   </GeneralDialog>
@@ -29,38 +18,17 @@ const invokeApiTestModules = computed(() => {
   const oriModules = builtInApiTestModules.value;
   oriModules.forEach((m) => {
     m?.dialog.args?.forEach((a) => {
+      //@ts-ignore
       if (a.options) {
+        //@ts-ignore
         if (typeof a.options === "function") {
+          //@ts-ignore
           a.options = a.options(listStore);
         }
       }
     });
   });
   return oriModules;
-  // return oriModules.map((m) => {
-  //   let dialog = m?.dialog;
-  //   if (dialog) {
-  //     dialog = {
-  //       ...dialog,
-  //       args: dialog?.args?.map((arg) => {
-  //         let opts = arg.options;
-  //         if (opts) {
-  //           if (typeof opts === "function") {
-  //             opts = opts(listStore);
-  //           }
-  //         }
-  //         return {
-  //           ...arg,
-  //           options: opts,
-  //         };
-  //       }),
-  //     };
-  //   }
-  //   return {
-  //     ...m,
-  //     dialog,
-  //   };
-  // });
 });
 </script>
 
