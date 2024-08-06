b1b17de7efe88ec959b217fdb1e606f6e90b134c
尘随风染 <2529720581@qq.com>
2024-08-06 20:47:04 +0800

    refactor: traversal uses key values with indexes to avoid errors and vscode is supported for opening files when passing file-path

--- src/components/script/RawCodeView.vue
+++ src/components/script/RawCodeView.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="code" overflow-x-hidden>
     <div class="code-content" overflow-y-scroll>
-      <div v-for="c in highlightCode" :key="c" v-html="c" text-nowrap cursor-text></div>
+      <div v-for="(c, index) in highlightCode" :key="c + index" v-html="c" text-nowrap cursor-text></div>
     </div>
-    <el-button class="copy-code" @click="copyCode(_rawCode)" size="small" v-if="showCopy"><el-icon>
-        <span i-mdi-content-copy></span>
-      </el-icon>复制</el-button>
+    <el-button-group class="btn-group">
+      <el-button @click="copyCode(_rawCode)" size="small" v-if="showCopy"><el-icon>
+          <span i-mdi-content-copy></span>
+        </el-icon>复制
+      </el-button>
+      <el-button @click="editCode(filePath)" size="small" v-if="filePath.trim().length"><el-icon>
+          <span i-bxl-visual-studio c-blue></span>
+        </el-icon>编辑
+      </el-button>
+    </el-button-group>
   </div>
 </template>
 <script lang="ts" setup>
 import { PropType } from 'vue';
 import { codeHighLight } from '../../utils/codeHighLight';
+import { invoke } from '@tauri-apps/api';
 const props = defineProps({
   //未使用codeHighLight处理过的代码
   rawCode: {
@@ -20,6 +28,10 @@ const props = defineProps({
   showCopy: {
     type: Boolean,
     default: true
+  },
+  filePath: {
+    type: String,
+    default: ''
   }
 });
 const _rawCode = computed(() => {
@@ -45,6 +57,9 @@ const copyCode = (text: string) => {
   }
 };
 const { appAsideBgColor } = useAppTheme();
+const editCode = (path: string) => {
+  invoke("open_file_explorer", { path });
+}
 </script>
 
 <style lang="scss" scoped>
@@ -55,16 +70,18 @@ const { appAsideBgColor } = useAppTheme();
   background: v-bind(appAsideBgColor);
   box-sizing: border-box;
   position: relative;
-  .code-content{
+
+  .code-content {
     user-select: text;
   }
+
   &:hover {
-    .copy-code {
+    .btn-group {
       display: block;
     }
   }
 
-  .copy-code {
+  .btn-group {
     display: none;
     position: absolute;
     right: 0;
