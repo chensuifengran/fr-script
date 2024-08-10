e3c06b861674ebe14ac4499be13c3f3ec1095d7e
尘随风染 <2529720581@qq.com>
2024-08-10 19:46:35 +0800

    feat: optimize the generation format of some templates, and support the application of the current configuration in the built-in API file

--- src/utils/invokeTemplate.ts
+++ src/utils/invokeTemplate.ts
@@ -1,3 +1,5 @@
+import { exists } from "@tauri-apps/api/fs";
+
 export type InvokeTemplateOptions = {
   name: string;
   document: ApiDocumentType;
@@ -42,6 +44,7 @@ export const defaultInvokeTemplateOptions =
   };
 export class InvokeTemplate {
   private options: Required<InvokeTemplateOptions>;
+  private contentCache: Record<string, string> = {};
   constructor(options: InvokeTemplateOptions) {
     options.scope = options.scope || "";
     options.weight = options.weight || 5;
@@ -66,7 +69,10 @@ export class InvokeTemplate {
                 : p.default
             },`;
           }
-          return `${p.name}${p.required ? "" : "?"}: ${_type},`;
+          if (_type.trim().length) {
+            return `${p.name}${p.required ? "" : "?"}: ${_type},`;
+          }
+          return `${p.name},`;
         })
         .join("\n  ");
     }
@@ -76,7 +82,10 @@ export class InvokeTemplate {
     if (!argList) {
       return "";
     } else {
-      return `${argList.map((i) => JSON.stringify(i)).join(", ")}`;
+      const res = JSON.stringify(argList, null, 2);
+      // 去除key中的双引号
+      const regex = /"([^"]+)":/g;
+      return res.replace(regex, "$1:");
     }
   }
   private genCBOptionNames() {
@@ -218,20 +227,22 @@ ${this.genRetract(tabCount)}}`;
   }
   get documentTemplate() {
     const d = this.options.document;
-    return `export const apiDocument = <ApiDocumentType>{
+    const prefixStr = `export const apiDocument = <ApiDocumentType>`;
+    const tempStr = `{
   howToUse: "${d.howToUse}",
   params: ${this.genParamListStr(d.params)},
   returnValue: {
     ${
       d?.returnValue?.instructions
-        ? `instructions: "${d.returnValue.instructions}",`
+        ? `instructions: "${d.returnValue.instructions}",\n    `
         : ""
-    }
-    type: \`${d?.returnValue?.type || ""}\`,
+    }type: \`${d?.returnValue?.type || ""}\`,
   },
   example: {
     ${
-      d?.example?.title ? `title: "${d.example.title.replace(/"/g, "'")}",` : ""
+      d?.example?.title
+        ? `title: "${d.example.title.replace(/"/g, "'")}",`
+        : 'title: "",'
     }
     code: \`${d?.example?.code || ""}\`,
   },
@@ -239,6 +250,16 @@ ${this.genRetract(tabCount)}}`;
   codeSnippet:
     "${d?.codeSnippet || this.defaultSnippet()}",
 };`;
+    try {
+      //对代码进行格式化
+      let res = JSON.stringify(eval(`const t = ${tempStr};t`), undefined, 2);
+      // 去除key中的双引号
+      const regex = /"([^"]+)":/g;
+      res = res.replace(regex, "$1:");
+      return `${prefixStr}${res}`;
+    } catch (error) {
+      return `${prefixStr}${tempStr}`;
+    }
   }
   get exportFnTemplate() {
     const name = this.options.name;
@@ -271,13 +292,16 @@ ${this.genRetract(tabCount)}}`;
       title ? `title: "${title}",\n  ` : ""
     }targetMethodName: "${this.options.name}",
   ${content ? `content: "${content}",\n  ` : ""}args: ${
-      args ? `[${this.genArgList()}]` : `[]`
+      args ? this.genArgList() : `[]`
     }
 };`;
   }
   get dialogCBTemplate() {
     const { name, scope, document, dialog } = this.options;
-    const newExample = document.example?.code.replace(/\(.*\)/, "(/*参数*/)");
+    const newExample = document.example?.code.replace(
+      /\(.*\)/,
+      `(${this.genCBOptionNames()})`
+    );
     if (dialog.notOpen) {
       return `export const modelCallback = async (
   _options: {
@@ -311,7 +335,7 @@ export const modelCallback = async (
   //测试调用时
   const { ${this.genCBOptionNames()} } = options;
   console.time("${name}耗时");
-  const res = await ${name}Fn(/*参数*/);
+  const res = await ${name}Fn(${this.genCBOptionNames()});
   console.time("${name}耗时");
   const selfModule = getInvokeApiMethods().find((i) => i.name === "${name}"${
       scope.length ? ` && i.scope === "${scope}"` : ""
@@ -330,7 +354,7 @@ export const modelCallback = async (
   //快速编辑/修改参数弹窗打开时触发
   onDialogOpen: async (_close,...args) => {
     //处理来自编辑器的参数
-    const params = await AutoTipUtils.paramsProcess(args);
+    const params = await AutoTipUtils.paramsProcess(...args);
     const selfModule = getInvokeApiMethods().find((i) => i.name === "${name}"${
       scope.length ? ` && i.scope === "${scope}"` : ""
     });
@@ -403,15 +427,426 @@ export const modelCallback = async (
         targetPath,
         "modelCallback.ts"
       );
-      await fsUtils.writeFile(indexFile, this.indexTemplate);
-      await fsUtils.writeFile(documentFile, this.documentTemplate);
-      await fsUtils.writeFile(exportFnFile, this.exportFnTemplate);
-      await fsUtils.writeFile(dialogFile, this.dialogTemplate);
-      await fsUtils.writeFile(auxiliaryFile, this.auxiliaryTemplate);
-      await fsUtils.writeFile(declarationFile, this.declaration);
-      await fsUtils.writeFile(modelCallbackFile, this.dialogCBTemplate);
+      await Promise.all([
+        fsUtils.writeFile(indexFile, this.indexTemplate),
+        fsUtils.writeFile(documentFile, this.documentTemplate),
+        fsUtils.writeFile(exportFnFile, this.exportFnTemplate),
+        fsUtils.writeFile(dialogFile, this.dialogTemplate),
+        fsUtils.writeFile(auxiliaryFile, this.auxiliaryTemplate),
+        fsUtils.writeFile(declarationFile, this.declaration),
+        fsUtils.writeFile(modelCallbackFile, this.dialogCBTemplate),
+      ]);
       return true;
     } catch (error) {
+      return false;
+    }
+  }
+  private async syncOtherFileName(apiIndexPath: string, oldName: string) {
+    const newName = this.options.name;
+    const oldEqReg = new RegExp(
+      `i\\.name\\s?={2,3}\\s?[\`"']${oldName}[\`"']`,
+      "g"
+    );
+    try {
+      //exportFn.ts文件
+      const exportFnPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../exportFn.ts"
+      );
+      let exportFnContent;
+      if (this.contentCache[exportFnPath]) {
+        exportFnContent = this.contentCache[exportFnPath];
+      } else {
+        exportFnContent = await fsUtils.readFile(exportFnPath);
+      }
+      const newExportFnContent = exportFnContent.replace(
+        new RegExp(`${oldName}Fn`, "g"),
+        newName + "Fn"
+      );
+      //auxiliary.ts文件
+      const auxiliaryPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../auxiliary.ts"
+      );
+      let auxiliaryContent;
+      if (this.contentCache[auxiliaryPath]) {
+        auxiliaryContent = this.contentCache[auxiliaryPath];
+      } else {
+        auxiliaryContent = await fsUtils.readFile(auxiliaryPath);
+      }
+      const newAuxiliaryContent = auxiliaryContent.replace(
+        oldEqReg,
+        `i.name === "${newName}"`
+      );
+      //declaration.ts文件
+      const declarationPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../declaration.ts"
+      );
+      let declarationContent;
+      if (this.contentCache[declarationPath]) {
+        declarationContent = this.contentCache[declarationPath];
+      } else {
+        declarationContent = await fsUtils.readFile(declarationPath);
+      }
+      const newDeclarationContent = declarationContent.replace(
+        new RegExp(`function ${oldName}`, "g"),
+        `function ${newName}`
+      );
+      //modelCallback.ts文件
+      const modelCallbackPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../modelCallback.ts"
+      );
+      let modelCallbackContent;
+      if (this.contentCache[modelCallbackPath]) {
+        modelCallbackContent = this.contentCache[modelCallbackPath];
+      } else {
+        modelCallbackContent = await fsUtils.readFile(modelCallbackPath);
+      }
+      let newModelCallbackContent = modelCallbackContent.replace(
+        new RegExp(`${oldName}Fn`, "g"),
+        `${newName}Fn`
+      );
+      newModelCallbackContent = newModelCallbackContent.replace(
+        new RegExp(`${oldName}耗时`, "g"),
+        `${newName}耗时`
+      );
+      newModelCallbackContent = newModelCallbackContent.replace(
+        oldEqReg,
+        `i.name === "${newName}"`
+      );
+      newModelCallbackContent = newModelCallbackContent.replace(
+        new RegExp(`"${oldName}"`, "g"),
+        `"${newName}"`
+      );
+      newModelCallbackContent = newModelCallbackContent.replace(
+        new RegExp(`\\.${oldName}`, "g"),
+        `.${newName}`
+      );
+      newModelCallbackContent = newModelCallbackContent.replace(
+        new RegExp(`${oldName}\\(`, "g"),
+        `${newName}(`
+      );
+      //document.ts文件
+      const documentPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../document.ts"
+      );
+      let documentContent;
+      if (this.contentCache[documentPath]) {
+        documentContent = this.contentCache[documentPath];
+      } else {
+        documentContent = await fsUtils.readFile(documentPath);
+      }
+      const newDocumentContent = documentContent.replace(
+        new RegExp(`${oldName}\\(`, "g"),
+        `${newName}(`
+      );
+      await Promise.all([
+        fsUtils.writeFile(exportFnPath, newExportFnContent),
+        fsUtils.writeFile(auxiliaryPath, newAuxiliaryContent),
+        fsUtils.writeFile(declarationPath, newDeclarationContent),
+        fsUtils.writeFile(modelCallbackPath, newModelCallbackContent),
+        fsUtils.writeFile(documentPath, newDocumentContent),
+      ]);
+      this.contentCache[exportFnPath] = exportFnContent;
+      this.contentCache[auxiliaryPath] = auxiliaryContent;
+      this.contentCache[declarationPath] = declarationContent;
+      this.contentCache[modelCallbackPath] = modelCallbackContent;
+      this.contentCache[documentPath] = documentContent;
+    } catch (error) {
+      console.error("syncOtherFileName error:", error);
+    }
+  }
+  private async syncOtherFileScope(apiIndexPath: string, oldScope: string) {
+    const newScope = this.options.scope;
+    const replaceReg = new RegExp(
+      `i\\.scope\\s?={2,3}\\s?[\`"']${oldScope}[\`"']`,
+      "g"
+    );
+    const newScopeExp = `i.scope === "${newScope}"`;
+    try {
+      //auxiliary.ts文件
+      const auxiliaryPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../auxiliary.ts"
+      );
+      let auxiliaryContent;
+      if (this.contentCache[auxiliaryPath]) {
+        auxiliaryContent = this.contentCache[auxiliaryPath];
+      } else {
+        auxiliaryContent = await fsUtils.readFile(auxiliaryPath);
+      }
+      const newAuxiliaryContent = auxiliaryContent.replace(
+        replaceReg,
+        newScopeExp
+      );
+
+      //modelCallback.ts文件
+      const modelCallbackPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../modelCallback.ts"
+      );
+      let modelCallbackContent;
+      if (this.contentCache[modelCallbackPath]) {
+        modelCallbackContent = this.contentCache[modelCallbackPath];
+      } else {
+        modelCallbackContent = await fsUtils.readFile(modelCallbackPath);
+      }
+      let newModelCallbackContent = modelCallbackContent.replace(
+        replaceReg,
+        `i.scope === "${newScope}"`
+      );
+      newModelCallbackContent = newModelCallbackContent.replace(
+        new RegExp(`${oldScope}\\.`, "g"),
+        `${newScope}.`
+      );
+      //document.ts文件
+      const documentPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../document.ts"
+      );
+      let documentContent;
+      if (this.contentCache[documentPath]) {
+        documentContent = this.contentCache[documentPath];
+      } else {
+        documentContent = await fsUtils.readFile(documentPath);
+      }
+      const newDocumentContent = documentContent.replace(
+        new RegExp(`${oldScope}\\.`, "g"),
+        `${newScope}.`
+      );
+      //declaration.ts文件
+      const declarationPath = await pathUtils.resolve(
+        apiIndexPath,
+        "../declaration.ts"
+      );
+      let declarationContent;
+      if (this.contentCache[declarationPath]) {
+        declarationContent = this.contentCache[declarationPath];
+      } else {
+        declarationContent = await fsUtils.readFile(declarationPath);
+      }
+      let newDeclarationContent = declarationContent;
+      if (this.options.scope.trim().length) {
+        newDeclarationContent = declarationContent.replace(/declare /, "");
+      } else if (!newDeclarationContent.includes("declare function")) {
+        newDeclarationContent = newDocumentContent.replace(
+          /function /,
+          "declare function "
+        );
+      }
+      await Promise.all([
+        fsUtils.writeFile(auxiliaryPath, newAuxiliaryContent),
+        fsUtils.writeFile(modelCallbackPath, newModelCallbackContent),
+        fsUtils.writeFile(documentPath, newDocumentContent),
+        fsUtils.writeFile(declarationPath, newDeclarationContent),
+      ]);
+      this.contentCache[auxiliaryPath] = auxiliaryContent;
+      this.contentCache[modelCallbackPath] = modelCallbackContent;
+      this.contentCache[documentPath] = documentContent;
+      this.contentCache[declarationPath] = declarationContent;
+    } catch (error) {
+      console.error("syncOtherFileScope error:", error);
+    }
+  }
+
+  private async applyIndexOptions(
+    apiIndexPath: string,
+    _oldOptions: InvokeTemplateOptions
+  ) {
+    const exist = await exists(apiIndexPath);
+    if (!exist) {
+      console.error(`文件${apiIndexPath}不存在`);
+      return;
+    }
+    let oldIndexContent;
+    if (this.contentCache[apiIndexPath]) {
+      oldIndexContent = this.contentCache[apiIndexPath];
+    } else {
+      oldIndexContent = await fsUtils.readFile(apiIndexPath);
+    }
+    const updateInfo = {
+      nameChanged: false,
+      scopeChanged: false,
+    };
+    let newContent = oldIndexContent;
+    const oldOptions = await astWorker.parseInvokeApiConfig(
+      oldIndexContent,
+      _oldOptions.name + "Api"
+    );
+    if (this.options.disabled) {
+      if (oldOptions.disabled === undefined) {
+        newContent = newContent.replace(
+          /name:\s*['"`].*['"`],?/,
+          `name: '${this.options.name}',\n  disabled: true,`
+        );
+      } else {
+        newContent = newContent.replace(
+          /disabled:\s*false,/,
+          `disabled: true,`
+        );
+      }
+    }
+    if (oldOptions.testModule.weight !== this.options.weight) {
+      newContent = newContent.replace(
+        /weight:\s*\d+,/,
+        `weight: ${this.options.weight},`
+      );
+    }
+    if (this.options.name !== oldOptions.name) {
+      newContent = newContent.replace(
+        /fn:\s*.*Fn,/,
+        `fn: ${this.options.name}Fn,`
+      );
+      newContent = newContent.replace(
+        /import\s+\{\s*.*Fn\s*\}\s+from\s+".\/exportFn";?/,
+        `import { ${this.options.name}Fn } from "./exportFn";`
+      );
+      newContent = newContent.replace(
+        /export const .*Api\s*=/,
+        `export const ${this.options.name}Api =`
+      );
+      newContent = newContent.replace(
+        /name:\s*['"`].*['"`],?/,
+        `name: '${this.options.name}',`
+      );
+      updateInfo.nameChanged = true;
+      await this.syncOtherFileName(apiIndexPath, oldOptions.name);
+    }
+    const changeScope = this.options.scope !== oldOptions.scope;
+    if (changeScope) {
+      if (oldOptions.scope !== undefined) {
+        newContent = newContent.replace(
+          /scope:\s*['"`].*['"`],?/,
+          this.options.scope.trim().length
+            ? `scope: '${this.options.scope}',`
+            : ""
+        );
+      } else {
+        if (this.options.scope.trim().length) {
+          newContent = newContent.replace(
+            /name:\s*['"`].*['"`],?/,
+            `scope: '${this.options.scope}',\n  name: '${this.options.name}',`
+          );
+        }
+      }
+      updateInfo.scopeChanged = true;
+      await this.syncOtherFileScope(apiIndexPath, oldOptions.scope);
+    }
+    newContent = newContent.replace(/^\s*\n/gm, "");
+    if (newContent !== oldIndexContent) {
+      await fsUtils.writeFile(apiIndexPath, newContent);
+      this.contentCache[apiIndexPath] = newContent;
+    }
+    return updateInfo;
+  }
+  private async applyDocumentOptions(documentFilePath: string) {
+    const exist = await exists(documentFilePath);
+    if (!exist) {
+      console.error(`文件${documentFilePath}不存在`);
+      return;
+    }
+    let oldDocumentContent;
+    if (this.contentCache[documentFilePath]) {
+      oldDocumentContent = this.contentCache[documentFilePath];
+    } else {
+      oldDocumentContent = await fsUtils.readFile(documentFilePath);
+    }
+    const oldCode = eval(
+      `${oldDocumentContent
+        .replace("<ApiDocumentType>", "")
+        .replace("export ", "")};apiDocument`
+    );
+    let newCode = eval(
+      `${this.documentTemplate
+        .replace("<ApiDocumentType>", "")
+        .replace("export ", "")};apiDocument`
+    );
+    if (JSON.stringify(oldCode) !== JSON.stringify(newCode)) {
+      await fsUtils.writeFile(documentFilePath, this.documentTemplate);
+      this.contentCache[documentFilePath] = this.documentTemplate;
+    }
+  }
+  private async applyDialogOptions(dialogFilePath: string) {
+    const exist = await exists(dialogFilePath);
+    if (!exist) {
+      console.error(`文件${dialogFilePath}不存在`);
+      return;
+    }
+    let oldDialogContent;
+    if (this.contentCache[dialogFilePath]) {
+      oldDialogContent = this.contentCache[dialogFilePath];
+    } else {
+      oldDialogContent = await fsUtils.readFile(dialogFilePath);
+    }
+    const oldCode = eval(
+      `${oldDialogContent
+        .replace("<TestModuleDialogType>", "")
+        .replace("export ", "")};dialogOptions`
+    );
+    let newCode = eval(
+      `${this.dialogTemplate
+        .replace("<TestModuleDialogType>", "")
+        .replace("export ", "")};dialogOptions`
+    );
+    if (JSON.stringify(oldCode) !== JSON.stringify(newCode)) {
+      await fsUtils.writeFile(dialogFilePath, this.dialogTemplate);
+      this.contentCache[dialogFilePath] = this.dialogTemplate;
+    }
+  }
+  async apply(apiIndexPath: string, oldOptions: InvokeTemplateOptions) {
+    try {
+      let invokesDir;
+      if (oldOptions.scope) {
+        invokesDir = await pathUtils.resolve(apiIndexPath, "../../../");
+      } else {
+        invokesDir = await pathUtils.resolve(apiIndexPath, "../../");
+      }
+      const oldDirPath = await pathUtils.resolve(apiIndexPath, "../");
+      const newDirPath = await pathUtils.resolve(
+        invokesDir,
+        `${this.options.scope.length ? this.options.scope + "/" : ""}${
+          this.options.name
+        }`
+      );
+      //判断新目录是否存在其他API
+      const exist = await exists(
+        await pathUtils.resolve(newDirPath, "index.ts")
+      );
+      if (exist) {
+        ElMessage.error(
+          `${this.options.scope ? this.options.scope + "." : ""}${
+            this.options.name
+          } 已存在,无法完成修改！`
+        );
+        return false;
+      }
+      const dialogFilePath = await pathUtils.resolve(
+        apiIndexPath,
+        "../dialog.ts"
+      );
+      const documentFilePath = await pathUtils.resolve(
+        apiIndexPath,
+        "../document.ts"
+      );
+      const iRes = await this.applyIndexOptions(apiIndexPath, oldOptions);
+      await this.applyDocumentOptions(documentFilePath);
+      await this.applyDialogOptions(dialogFilePath);
+      if (iRes?.nameChanged || iRes?.scopeChanged) {
+        const res = await fsUtils.moveChildToNewDir(oldDirPath, newDirPath);
+        if (res) {
+          ElMessage.success("更新成功");
+        } else {
+          console.error("更新失败", oldDirPath, newDirPath, this.options.scope);
+          ElMessage.error("更新失败");
+          return false;
+        }
+      }
+      return true;
+    } catch (error) {
+      console.error("apply new options error:", error);
       return false;
     }
   }
