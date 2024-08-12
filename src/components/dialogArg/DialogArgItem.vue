34d047e75d0b451b54895ab180f2a8859ab4a89c
尘随风染 <2529720581@qq.com>
2024-08-12 15:08:14 +0800

    refactor: the component style is displayed only in edit mode, otherwise only the component type and the component name are displayed

--- src/components/dialogArg/DialogArgItem.vue
+++ src/components/dialogArg/DialogArgItem.vue
@@ -6,133 +6,150 @@
         <el-text class="info">{{ argItem.name }}</el-text>
       </div>
       <el-button-group>
-        <el-button @click="emitEdit(argItem.id!)" link class="btn" type="primary">
-          <el-icon class="btn"><span i-mdi-playlist-edit class="btn"></span></el-icon>
+        <el-button
+          @click="emitEdit(argItem.id!)"
+          link
+          class="btn"
+          type="primary"
+        >
+          <el-icon class="btn"
+            ><span i-mdi-playlist-edit class="btn"></span
+          ></el-icon>
         </el-button>
-        <el-button type="danger" @click="emitDelete(argItem.id!)" link class="btn">
-          <el-icon class="btn"><span i-mdi-delete-outline class="btn"></span></el-icon>
+        <el-button
+          type="danger"
+          @click="emitDelete(argItem.id!)"
+          link
+          class="btn"
+        >
+          <el-icon class="btn"
+            ><span i-mdi-delete-outline class="btn"></span
+          ></el-icon>
         </el-button>
       </el-button-group>
     </div>
     <div v-else class="mask-box show-mask"></div>
-    <span class="label" v-if="argItem?.label !== undefined && !notShowType.includes(argItem.componentType)">
+    <span
+      class="label"
+      v-if="
+        argItem?.label !== undefined &&
+        !notShowType.includes(argItem.componentType) &&
+        edit
+      "
+    >
       {{ argItem.label }}
     </span>
-    <template v-if="argItem && argItem.componentType === 'select'">
-      <el-select v-model="argItem.value" filterable :multiple="argItem.multiple" :clearable="argItem.multiple"
-        :allow-create="!argItem.notAllowCreate" default-first-option :placeholder="argItem.placeholder || '请选择'">
-      </el-select>
-    </template>
-    <template v-else-if="
-      argItem &&
-      argItem.componentType === 'FileInput'
-    ">
-      <FileInput v-model="argItem.value" :label="argItem.label" :verify="argItem.verifyPath"
-        :multiple="argItem.multiple" />
-    </template>
-    <template v-else-if="
-      argItem && argItem.componentType === 'RectInput'
-    ">
-      <RectInput v-model="argItem.value" />
-    </template>
-    <template v-else-if="
-      argItem && argItem.componentType === 'slider'
-    ">
-      <SliderInput v-model="argItem.value" :max="argItem.range?.max" :min="argItem.range?.min"
-        :step="argItem.range?.step" :label="argItem.label" size="small" :width="100"/>
-    </template>
-    <template v-else-if="
-      argItem && argItem.componentType === 'switch'
-    ">
-      <el-switch v-model="argItem.value" :active-text="argItem.activeText || '是'"
-        :inactive-text="argItem.inactiveText || '否'" size="small" />
-    </template>
-    <template v-else-if="
-      argItem && argItem.componentType === 'DirInput'
-    ">
-      <DirInput v-model="argItem.value" :label="argItem.label" :suffix="argItem.suffix || ''"
-        :verify="argItem.verifyPath" />
-    </template>
-    <template v-else-if="
-      argItem && argItem.componentType === 'numberInput'
-    ">
-      <el-input-number v-model="argItem.value" :value-on-clear="0" size="small" />
-    </template>
-    <template v-else-if="
-      argItem && argItem.componentType === 'input'
-    ">
-      <el-input v-model="argItem.value" size="small">
-        <template #prepend> {{ argItem.label }} </template>
-      </el-input>
-    </template>
-    <template v-else-if="
-      argItem &&
-      argItem.componentType === 'numberRangeInput'
-    ">
-      <RangeInput v-model="argItem.value" />
+    <div v-if="!edit" flex flex-row flex-items-center>
+      <el-tag type="primary" size="small">{{ argItem.componentType }}</el-tag
+      ><el-text>{{ argItem.label }}</el-text>
+    </div>
+    <template v-else>
+      <template v-if="argItem && argItem.componentType === 'select'">
+        <el-select
+          v-model="argItem.value"
+          filterable
+          :multiple="argItem.multiple"
+          :clearable="argItem.multiple"
+          :allow-create="!argItem.notAllowCreate"
+          default-first-option
+          :placeholder="argItem.placeholder || '请选择'"
+        >
+        </el-select>
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'FileInput'">
+        <FileInput
+          v-model="argItem.value"
+          :label="argItem.label"
+          :verify="argItem.verifyPath"
+          :multiple="argItem.multiple"
+        />
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'RectInput'">
+        <RectInput v-model="argItem.value" />
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'slider'">
+        <SliderInput
+          v-model="argItem.value"
+          :max="argItem.range?.max"
+          :min="argItem.range?.min"
+          :step="argItem.range?.step"
+          :label="argItem.label"
+          size="small"
+          :width="100"
+        />
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'switch'">
+        <el-switch
+          v-model="argItem.value"
+          :active-text="argItem.activeText || '是'"
+          :inactive-text="argItem.inactiveText || '否'"
+          size="small"
+        />
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'DirInput'">
+        <DirInput
+          v-model="argItem.value"
+          :label="argItem.label"
+          :suffix="argItem.suffix || ''"
+          :verify="argItem.verifyPath"
+        />
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'numberInput'">
+        <el-input-number
+          v-model="argItem.value"
+          :value-on-clear="0"
+          size="small"
+        />
+      </template>
+      <template v-else-if="argItem && argItem.componentType === 'input'">
+        <el-input v-model="argItem.value" size="small">
+          <template #prepend> {{ argItem.label }} </template>
+        </el-input>
+      </template>
+      <template
+        v-else-if="argItem && argItem.componentType === 'numberRangeInput'"
+      >
+        <RangeInput v-model="argItem.value" />
+      </template>
     </template>
   </div>
 </template>
 <script lang="ts" setup>
-// import { ListStore } from '../../store/listStore';
 defineProps({
   argItem: {
     type: Object as PropType<DialogDynamicArgItem>,
-    required: true
+    required: true,
   },
   edit: {
     type: Boolean,
-    default: false
+    default: false,
   },
 });
 const emit = defineEmits<{
-  onEdit: [id: string],
-  onDelete: [id: string]
+  onEdit: [id: string];
+  onDelete: [id: string];
 }>();
 const itemClass = (componentType: string) => {
-  const rowType = [
-    'switch', 'numberInput', 'numberRangeInput'
-  ]
+  const rowType = ["switch", "numberInput", "numberRangeInput"];
   if (rowType.includes(componentType)) {
-    return "row-item arg-item"
+    return "row-item arg-item";
   } else {
-    return 'arg-item'
+    return "arg-item";
   }
-}
-// const parseOption = (item: string | number, selectOptionSeparator?: string) => {
-//   if (typeof item === "number" || !selectOptionSeparator) {
-//     return {
-//       label: item,
-//       value: item,
-//     };
-//   }
-//   const arr = item.split(selectOptionSeparator);
-//   return {
-//     label: arr[0],
-//     value: arr[1] || arr[0],
-//   };
-// };
-// const transfromFnOption = (options: string[] | number[] | ((store: ListStore) => string[] | number[])) => {
-//   if (typeof options === 'function') {
-//     return options(useListStore())
-//   } else {
-//     return options
-//   }
-// }
+};
 const notShowType = ["input", "FileInput", "DirInput", "slider"];
-const {
-  appAsideBgColor
-} = useAppTheme();
+const { appAsideBgColor } = useAppTheme();
 const maskBg = computed(() => {
-  const appAsideBgColorReverse = appAsideBgColor.value === '#272727' ? '#f6f6f6' : '#272727';
+  const appAsideBgColorReverse =
+    appAsideBgColor.value === "#272727" ? "#f6f6f6" : "#272727";
   return appAsideBgColorReverse + "B0";
-})
+});
 const emitEdit = (id: string) => {
-  emit('onEdit', id);
-}
+  emit("onEdit", id);
+};
 const emitDelete = (id: string) => {
-  emit('onDelete', id);
-}
+  emit("onDelete", id);
+};
 </script>
 
 <style lang="scss" scoped>
@@ -185,7 +202,7 @@ const emitDelete = (id: string) => {
   }
 
   &::after {
-    content: '';
+    content: "";
     width: 4px;
     position: absolute;
     left: 0;
