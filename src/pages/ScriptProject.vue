c9ec13beb87fe61c76d7a1c0ad81f089f0215cc9
尘随风染 <2529720581@qq.com>
2024-08-21 16:14:31 +0800

    chore: v2.0.6

--- src/pages/ScriptProject.vue
+++ src/pages/ScriptProject.vue
@@ -1,74 +1,64 @@
 <template>
-  <div class="flex">
-    <VueDraggable
-      v-model="list1"
-      :animation="150"
-      ghostClass="ghost"
-      :group="{ name: 'people', pull: 'clone', put: false }"
-      :clone="clone"
-      :sort="false"
-      class="flex flex-col gap-2 p-4 w-300px bg-gray-500/5 rounded"
-    >
-      <div
-        v-for="item in list1"
-        :key="item.id"
-        class="cursor-move h-50px bg-gray-500/5 rounded p-3"
-      >
+  <div w-full h-full pos-relative flex items-center justify-center class="container-root">
+    <!-- <VueDraggable v-model="list1" :animation="150" ghostClass="ghost"
+      :group="{ name: 'people', pull: 'clone', put: false }" :clone="clone" :sort="false"
+      class="flex flex-col gap-2 p-4 w-300px bg-gray-500/5 rounded">
+      <div v-for="item in list1" :key="item.id" class="cursor-move h-50px bg-gray-500/5 rounded p-3">
         {{ item.name }}
       </div>
     </VueDraggable>
-    <VueDraggable
-      v-model="list2"
-      :animation="150"
-      group="people"
-      ghostClass="ghost"
-      class="flex flex-col gap-2 p-4 w-300px max-h-350px m-auto bg-gray-500/5 rounded overflow-auto"
-    >
-      <div
-        v-for="item in list2"
-        :key="item.id"
-        class="cursor-move h-50px bg-gray-500/5 rounded p-3"
-      >
+    <VueDraggable v-model="list2" :animation="150" group="people" ghostClass="ghost"
+      class="flex flex-col gap-2 p-4 w-300px max-h-350px m-auto bg-gray-500/5 rounded overflow-auto">
+      <div v-for="item in list2" :key="item.id" class="cursor-move h-50px bg-gray-500/5 rounded p-3">
         {{ item.name }}
       </div>
-    </VueDraggable>
+    </VueDraggable> -->
+    <el-empty description="开发中(新建文件夹阶段)" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { VueDraggable } from 'vue-draggable-plus'
+// import { VueDraggable } from 'vue-draggable-plus'
 
-const list1 = ref([
-  {
-    name: 'Joao',
-    id: '1'
-  },
-  {
-    name: 'Jean',
-    id: '2'
-  },
-  {
-    name: 'Johanna',
-    id: '3'
-  },
-  {
-    name: 'Juan',
-    id: '4'
-  }
-])
-const list2 = ref(
-  list1.value.map(item => ({
-    name: `${item.name}-2`,
-    id: `${item.id}-2`
-  }))
-)
+// const list1 = ref([
+//   {
+//     name: 'Joao',
+//     id: '1'
+//   },
+//   {
+//     name: 'Jean',
+//     id: '2'
+//   },
+//   {
+//     name: 'Johanna',
+//     id: '3'
+//   },
+//   {
+//     name: 'Juan',
+//     id: '4'
+//   }
+// ])
+// const list2 = ref(
+//   list1.value.map(item => ({
+//     name: `${item.name}-2`,
+//     id: `${item.id}-2`
+//   }))
+// )
 
-function clone(element: Record<'name' | 'id', string>) {
-  const len = list2.value.length
-  return {
-    name: `${element.name}-clone-${len}`,
-    id: `${element.id}-clone-${len}`
-  }
-}
+// function clone(element: Record<'name' | 'id', string>) {
+//   const len = list2.value.length
+//   return {
+//     name: `${element.name}-clone-${len}`,
+//     id: `${element.id}-clone-${len}`
+//   }
+// }
+const { appBackground } = useAppTheme();
+
 </script>
+
+<style lang="scss" scoped>
+.container-root {
+  background: v-bind(appBackground);
+  border-radius: 10px 10px 10px 0;
+}
+</style>
