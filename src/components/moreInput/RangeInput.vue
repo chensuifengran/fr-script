8e674abe325fb738c4fd69ac2866e80ec369e668
尘随风染 <2529720581@qq.com>
2024-08-24 20:34:14 +0800

    refactor: support for label display

--- src/components/moreInput/RangeInput.vue
+++ src/components/moreInput/RangeInput.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="range-input">
-    <el-tooltip class="box-item" effect="dark" content="最小值" placement="bottom">
-      <el-input-number class="el-ipt" v-model="model![0]" :min="limit ? limit[0] : undefined" :max="lowMax" size="small"
-        :controls="false" @change="handleChange()" :disabled="disabled" />
-    </el-tooltip>
-    ~
-    <el-tooltip class="box-item" effect="dark" content="最大值" placement="bottom">
-      <el-input-number class="el-ipt" v-model="model![1]" :min="min" :max="limit ? limit[1] : undefined" size="small"
-        :controls="false" @change="handleChange()" :disabled="disabled" />
-    </el-tooltip>
+  <div
+    flex
+    flex-row
+    items-center
+    :style="{
+      justifyContent: label ? 'space-between' : 'flex-start',
+    }"
+  >
+    <div v-if="label">{{ label }}</div>
+    <div
+      flex
+      flex-row
+      items-center
+      :style="{
+        width: label ? 'auto' : '100%',
+      }"
+    >
+      <el-tooltip effect="dark" content="最小值" placement="bottom">
+        <el-input-number
+          w-65px
+          v-model="model![0]"
+          :min="limit ? limit[0] : undefined"
+          :max="lowMax"
+          size="small"
+          :controls="false"
+          @change="handleChange()"
+          :disabled="disabled"
+        />
+      </el-tooltip>
+      ~
+      <el-tooltip effect="dark" content="最大值" placement="bottom">
+        <el-input-number
+          w-65px
+          v-model="model![1]"
+          :min="min"
+          :max="limit ? limit[1] : undefined"
+          size="small"
+          :controls="false"
+          @change="handleChange()"
+          :disabled="disabled"
+        />
+      </el-tooltip>
+    </div>
   </div>
 </template>
 
@@ -29,7 +62,11 @@ const props = defineProps({
   disabled: {
     type: Boolean,
     default: false,
-  }
+  },
+  label: {
+    type: String,
+    default: "",
+  },
 });
 const handleChange = () => {
   if (model.value[0] >= model.value[1]) {
@@ -49,9 +86,4 @@ onMounted(() => {
 </script>
 
 <style scoped lang="scss">
-.range-input {
-  .el-ipt {
-    width: 65px;
-  }
-}
 </style>
