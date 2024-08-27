5253ed56174f5c9f1e7566a5ae29f1d819925e3c
尘随风染 <2529720581@qq.com>
2024-08-27 15:32:05 +0800

    feat: edit box prompts can display the maximum value in the limit option, props added "controls" option

--- src/components/moreInput/RangeInput.vue
+++ src/components/moreInput/RangeInput.vue
@@ -16,27 +16,35 @@
         width: label ? 'auto' : '100%',
       }"
     >
-      <el-tooltip effect="dark" content="最小值" placement="bottom">
+      <el-tooltip
+        effect="dark"
+        :content="'最小值' + (limit ? `(min:${limit[0]})` : '')"
+        placement="bottom"
+      >
         <el-input-number
-          w-65px
+          :class="controls ? 'w-105px' : 'w-65px'"
           v-model="model![0]"
           :min="limit ? limit[0] : undefined"
           :max="lowMax"
           size="small"
-          :controls="false"
+          :controls="controls"
           @change="handleChange()"
           :disabled="disabled"
         />
       </el-tooltip>
       ~
-      <el-tooltip effect="dark" content="最大值" placement="bottom">
+      <el-tooltip
+        effect="dark"
+        :content="'最大值' + (limit ? `(max:${limit[1]})` : '')"
+        placement="bottom"
+      >
         <el-input-number
-          w-65px
+          :class="controls ? 'w-105px' : 'w-65px'"
           v-model="model![1]"
           :min="min"
           :max="limit ? limit[1] : undefined"
           size="small"
-          :controls="false"
+          :controls="controls"
           @change="handleChange()"
           :disabled="disabled"
         />
@@ -67,6 +75,10 @@ const props = defineProps({
     type: String,
     default: "",
   },
+  controls:{
+    type: Boolean,
+    default: false
+  }
 });
 const handleChange = () => {
   if (model.value[0] >= model.value[1]) {
@@ -85,5 +97,4 @@ onMounted(() => {
 });
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
