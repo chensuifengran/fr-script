45257a17715e7f3b811f489067ed2333e86c04b9
尘随风染 <2529720581@qq.com>
2024-08-28 15:27:00 +0800

    feat: supports simultaneous changes to rendererListDeclare.ts and buildFormDeclare.ts when rendererList.d.ts and buildformdeclare.ts are changed

--- vitePlugins/hotUpdate.ts
+++ vitePlugins/hotUpdate.ts
@@ -12,30 +12,55 @@ const runScript = (type: "api" | "declare") => {
   timer && clearTimeout(timer);
   timer = setTimeout(() => {
     //使用node执行scriptPath脚本
-    exec(
-      `node ${scriptPath} ${type}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`执行错误: ${error}`);
-          return;
-        }
-        //HH:mm:ss
-        const date = new Date().toLocaleTimeString().slice(0, 8);
-        if (stderr) {
-          console.error(
-            date,
-            chalk.blue("[hot-update-plugin]"),
-            chalk.red(stderr)
-          );
-        } else {
-          console.log(
-            date,
-            chalk.blue("[hot-update-plugin]"),
-            chalk.green(stdout)
-          );
-        }
+    exec(`node ${scriptPath} ${type}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行错误: ${error}`);
+        return;
       }
-    );
+      //HH:mm:ss
+      const date = new Date().toLocaleTimeString().slice(0, 8);
+      if (stderr) {
+        console.error(
+          date,
+          chalk.blue("[hot-update-plugin]"),
+          chalk.red(stderr)
+        );
+      } else {
+        console.log(
+          date,
+          chalk.blue("[hot-update-plugin]"),
+          chalk.green(stdout)
+        );
+      }
+    });
+  }, 500);
+};
+const genDeclareScriptPath = resolve(__dirname, "../script/genApiDeclare.js");
+let genApiDeclareTimer: any;
+const getDeclareStr = (target: "buildForm" | "rendererList") => {
+  genApiDeclareTimer && clearTimeout(genApiDeclareTimer);
+  genApiDeclareTimer = setTimeout(() => {
+    exec(`node ${genDeclareScriptPath} ${target}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行错误: ${error}`);
+        return;
+      }
+      //HH:mm:ss
+      const date = new Date().toLocaleTimeString().slice(0, 8);
+      if (stderr) {
+        console.error(
+          date,
+          chalk.blue("[hot-update-plugin]"),
+          chalk.red(stderr)
+        );
+      } else {
+        console.log(
+          date,
+          chalk.blue("[hot-update-plugin]"),
+          chalk.green(stdout)
+        );
+      }
+    });
   }, 500);
 };
 export function hotUpdatePlugin(): Plugin {
@@ -43,14 +68,25 @@ export function hotUpdatePlugin(): Plugin {
     name: "hot-update-plugin",
     handleHotUpdate({ file }) {
       if (file.endsWith(".ts")) {
-        if (file.includes("BuiltInApiType.ts") || file.includes("core.d.ts")) {
+        const excludeFiles = [
+          "builtInApi.d.ts",
+          "core.d.ts",
+          "buildFormDeclare.ts",
+          "rendererListDeclare.ts",
+        ];
+        if (excludeFiles.find((e) => file.includes(e))) {
           return;
         }
-        if(file.includes('invokes/')){
+        if (file.includes("invokes/")) {
           runScript("api");
         }
-        if(file.includes('useScriptApi.ts') || file.includes('useCore.ts')){
+        if (file.includes("useScriptApi.ts") || file.includes("useCore.ts")) {
           runScript("declare");
+        }
+        if (file.includes("buildForm.d.ts")) {
+          getDeclareStr("buildForm");
+        } else if (file.includes("rendererList.d.ts")) {
+          getDeclareStr("rendererList");
         }
       }
     },
