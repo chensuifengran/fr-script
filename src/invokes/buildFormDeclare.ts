2665dc1a1c013b2844c3494148fc047c8825ea8b
尘随风染 <2529720581@qq.com>
2024-09-04 18:28:25 +0800

    feat: add Picker type to BuildFormItems

--- src/invokes/buildFormDeclare.ts
+++ src/invokes/buildFormDeclare.ts
@@ -3,26 +3,29 @@ declare namespace BuildFormItem {
   type Base = {
     targetGroupLabel: string;
     enable?: boolean;
+    label: string;
   };
   type StripBase<T> = {
     [P in keyof T as Exclude<P, keyof Base>]: T[P];
   };
   type Input = Base & {
     type: "input";
-    label: string;
   } & InputListItem;
   type Select = Base & {
     type: "select";
-    label: string;
   } & SelectListItem;
   type Check = Base & {
     type: "check";
-    label: string;
     checked: boolean;
   };
+  type Picker = Base & {
+    type: "picker";
+  } & PickerListItem;
 }
+
 declare type BuildFormItems =
   | BuildFormItem.Input
   | BuildFormItem.Select
-  | BuildFormItem.Check;
+  | BuildFormItem.Check
+  | BuildFormItem.Picker;
 `
