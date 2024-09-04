53882571347150020560937f80bc3657ba66641b
尘随风染 <2529720581@qq.com>
2024-09-04 18:32:43 +0800

    feat: add pickerList to the rendererList group and add PTime, PDate, and Color to the PickerListItem type

--- src/types/rendererList.d.ts
+++ src/types/rendererList.d.ts
@@ -31,7 +31,8 @@ declare type OptionItem<T extends string | number | boolean = string> = {
   label: string;
   value: T;
 };
-declare type IdField = {
+declare type BaseListItem = {
+  label: string;
   id?: string;
 };
 declare type TextInputItem = {
@@ -44,7 +45,7 @@ declare type TextInputItem = {
   showWordLimit?: boolean;
   autosize?: [number | undefined, number | undefined] | number | boolean;
 } & OptionItem<string> &
-  IdField;
+  BaseListItem;
 declare type NumberInputItem = {
   inputType: "number";
   min?: number;
@@ -56,41 +57,105 @@ declare type NumberInputItem = {
   controls?: boolean;
   valueOnClear?: number | "max" | "min";
 } & OptionItem<number> &
-  IdField;
+  BaseListItem;
 declare type RangeInputItem = {
   inputType: "range";
-  label: string;
   value: [number, number];
   limit?: [number, number];
   controls?: boolean;
-} & IdField;
-type FileInputItem<Multiple extends boolean> = {
+} & BaseListItem;
+declare type FileInputItem<Multiple extends boolean> = {
   inputType: "file";
-  label: string;
   multiple: Multiple extends true ? true : false | undefined;
   value: Multiple extends true ? string[] : string;
-} & IdField;
+} & BaseListItem;
 declare type InputListItem =
   | TextInputItem
   | NumberInputItem
   | RangeInputItem
   | FileInputItem<true>
   | FileInputItem<false>;
-declare type BaseSelectItem<T extends string | number | boolean> = {
-  label: string;
-} & (
+declare type BaseSelectItem<T extends string | number | boolean> = (
   | SelectType.Default<T>
   | SelectType.Base<T, SelectType.ConstantOption<T>, SelectType.Multiple<T>>
   | SelectType.Base<T, SelectType.GroupOption<T>, SelectType.Single<T>>
   | SelectType.Base<T, SelectType.GroupOption<T>, SelectType.Multiple<T>>
 ) &
-  IdField;
+  BaseListItem;
 declare type SelectListItem =
   | BaseSelectItem<string>
   | BaseSelectItem<number>
   | BaseSelectItem<boolean>;
-
-declare type CheckListItem = { label: string; checked: boolean } & IdField;
+declare type CheckListItem = { checked: boolean } & BaseListItem;
+declare namespace PickerItem {
+  type PTime = {
+    pickerType: "time";
+    clearable?: boolean;
+    format?: string;
+    disabledHours?: (role: string, comparingDate?: any) => number[];
+    disabledMinutes?: (
+      hour: number,
+      role: string,
+      comparingDate?: any
+    ) => number[];
+    disabledSeconds?: (
+      hour: number,
+      minute: number,
+      role: string,
+      comparingDate?: any
+    ) => number[];
+  } & (
+    | {
+        isRange?: false;
+        value: Date;
+        placeholder?: string;
+      }
+    | {
+        isRange: false;
+        valueFormat: string;
+        value: string;
+        placeholder?: string;
+      }
+    | {
+        isRange: true;
+        valueFormat: string;
+        value: [string, string];
+        rangeSeparator?: string;
+        startPlaceholder?: string;
+        endPlaceholder?: string;
+      }
+    | {
+        isRange: true;
+        value: [Date, Date];
+        rangeSeparator?: string;
+        startPlaceholder?: string;
+        endPlaceholder?: string;
+      }
+  );
+  type PDate = {
+    pickerType: "date";
+    value: Date;
+  };
+  type Color<Alpha extends boolean> = {
+    pickerType: "color";
+    enableAlpha?: Alpha;
+    colorFormat?:
+      | "hsl"
+      | "hsv"
+      | "hex"
+      | "rgb"
+      | (Alpha extends true ? "hsla" | "hsva" | "hexa" | "rgba" : undefined);
+    predefine?: string[];
+    value: string;
+  };
+}
+declare type PickerListItem = (
+  | PickerItem.PTime
+  | PickerItem.PDate
+  | PickerItem.Color<true>
+  | PickerItem.Color<false>
+) &
+  BaseListItem;
 declare type RendererList = {
   id?: string;
   groupLabel: string;
@@ -98,4 +163,5 @@ declare type RendererList = {
   checkList: CheckListItem[];
   inputList: InputListItem[];
   selectList: SelectListItem[];
+  pickerList: PickerListItem[];
 };
