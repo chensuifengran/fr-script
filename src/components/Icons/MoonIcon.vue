986bfb0a12665727a82860bd3d7f22cde21d10af
尘随风染 <2529720581@qq.com>
2024-09-04 13:26:14 +0800

    feat: replace the icon corresponding to @elements-plus /icons-vue

---
+++ src/components/Icons/MoonIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-icon size="small">
+    <span i-solar-moon-bold></span>
+  </el-icon>
+</template>
+<script lang="ts" setup></script>
---
+++ src/components/Icons/SunIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <el-icon size="small">
+    <span i-solar-sun-2-bold></span>
+  </el-icon>
+</template>
+<script lang="ts" setup></script>
