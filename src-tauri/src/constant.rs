4e7ca848b1b3c8b456cb8dc1f5e757d4ae541ba1
尘随风染 <2529720581@qq.com>
2024-09-22 17:00:59 +0800

    doc: add doc

--- src-tauri/src/constant.rs
+++ src-tauri/src/constant.rs
@@ -1,3 +1,8 @@
+/**
+ * 讯飞星火API配置，lite版不限量 
+ * 申请地址：https://console.xfyun.cn/services/cbm 
+ * 申请后将APP_ID、API_KEY、API_SECRET替换成自己的
+ */
 pub const APP_ID: &str = "8246b766";
 
 pub const API_KEY: &str = "d2adf59014f9bde0d4c0b49c5eb97cd4";
