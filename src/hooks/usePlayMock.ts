8f29b111ca39c521174bedc195593b0e96281da6
尘随风染 <2529720581@qq.com>
2024-09-24 17:03:46 +0800

    feat: add mockCodeSnippetList

--- src/hooks/usePlayMock.ts
+++ src/hooks/usePlayMock.ts
@@ -2,8 +2,17 @@ import { useLocalStorageState } from "vue-hooks-plus";
 export type MockScriptListItem = ScriptItemType & {
   content?: string;
 };
+export type MockCodeSnippet = CodeSnippet & {
+  content?: string;
+};
 const [list, setList] = useLocalStorageState<MockScriptListItem[]>(
   "mockScriptList",
+  {
+    defaultValue: [],
+  }
+);
+const [codeList, setCodeList] = useLocalStorageState<MockCodeSnippet[]>(
+  "mockCodeSnippetList",
   {
     defaultValue: [],
   }
@@ -11,14 +20,31 @@ const [list, setList] = useLocalStorageState<MockScriptListItem[]>(
 const mockScriptList = ref<MockScriptListItem[]>(
   JSON.parse(JSON.stringify(list.value)) || []
 );
-watch(mockScriptList, () => {
-  setList(mockScriptList.value);
-},{
-  deep: true
-});
+const mockCodeSnippetList = ref<MockCodeSnippet[]>(
+  JSON.parse(JSON.stringify(codeList.value)) || []
+);
+watch(
+  mockScriptList,
+  () => {
+    setList(mockScriptList.value);
+  },
+  {
+    deep: true,
+  }
+);
+watch(
+  mockCodeSnippetList,
+  () => {
+    setCodeList(mockCodeSnippetList.value);
+  },
+  {
+    deep: true,
+  }
+);
 
 export const usePlayMock = () => {
   return {
     mockScriptList,
+    mockCodeSnippetList,
   };
 };
