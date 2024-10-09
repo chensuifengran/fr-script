32e48a007e2c5368f1383b8dc106be06cee772fc
尘随风染 <2529720581@qq.com>
2024-10-09 23:19:48 +0800

    feat: captured error information to be displayed in the script log box

--- src/utils/logUtil.ts
+++ src/utils/logUtil.ts
@@ -1,35 +1,32 @@
-import { invoke } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/core";
+import { nanoid } from "nanoid";
 const isProduction = import.meta.env.PROD;
 let isSetReport = false;
 const setProductionErrorReport = () => {
-  if (IS_PLAYGROUND_ENV) {
-    return;
-  }
-  if (isProduction && !isSetReport) {
-    const oldConsoleProto = window.console;
-    const newConsoleProto = Object.create(oldConsoleProto);
-    newConsoleProto.error = function (...data: any[]) {
-      data.forEach((d) => {
-        invoke("error_report", {
-          msg: d,
-        });
+  const oldConsoleProto = window.console;
+  const newConsoleProto = Object.create(oldConsoleProto);
+  newConsoleProto.error = function (...data: any[]) {
+    const msg = data
+      .map((d) => (typeof d === "string" ? d : JSON.stringify(d)))
+      .join(" ");
+    if (!IS_PLAYGROUND_ENV && isProduction && !isSetReport) {
+      invoke("error_report", {
+        msg,
       });
-      oldConsoleProto.error.call(this, ...data);
-    };
-    window.console = newConsoleProto;
-    isSetReport = true;
-  }
-};
-const scriptConsoleErrorReport = (msg: string, name?: string) => {
-  if (IS_PLAYGROUND_ENV) {
-    return;
-  }
-  if (isProduction) {
-    invoke("error_report", {
-      msg: `[${name || "脚本控制台"}]${msg}`,
+    }
+    useLog().logOutput.push({
+      id: "SYS-" + nanoid(),
+      time: new Date(Date.now()).toLocaleTimeString(),
+      log: msg,
+      type: "danger",
+      timestamp: Date.now(),
     });
-  }
+    oldConsoleProto.error.call(this, ...data);
+  };
+  window.console = newConsoleProto;
+  isSetReport = true;
 };
+
 const report = (msg: any) => {
   if (IS_PLAYGROUND_ENV) {
     return;
@@ -37,6 +34,14 @@ const report = (msg: any) => {
   return invoke<void>("error_report", {
     msg: JSON.stringify(msg),
   });
+};
+const scriptConsoleErrorReport = (msg: string, name?: string) => {
+  if (IS_PLAYGROUND_ENV) {
+    return;
+  }
+  if (isProduction) {
+    report(`[${name || "脚本控制台"}]${msg}`);
+  }
 };
 export const logUtil = {
   setProductionErrorReport,
