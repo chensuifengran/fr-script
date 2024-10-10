bc669439e46718595aad420bec1b6c3bc745b679
尘随风染 <2529720581@qq.com>
2024-10-10 10:48:44 +0800

    feat: add return-icon

---
+++ src/components/Icons/ReturnIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-icon :size="size"><span i-solar-undo-left-bold></span></el-icon>
+</template>
+<script lang="ts" setup>
+defineProps({
+  size: {
+    type: String,
+    default: "large",
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
