279e092c46bbff89a2013080067ad3c812a2f649
尘随风染 <2529720581@qq.com>
2024-10-11 23:39:42 +0800

    refactor: add default mock script and code snippet
    add default presentation scripts and default code snippets to the playground's mock list

--- src/hooks/usePlayMock.ts
+++ src/hooks/usePlayMock.ts
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { useLocalStorageState } from "vue-hooks-plus";
 export type MockScriptListItem = ScriptItemType & {
   content?: string;
@@ -8,13 +9,38 @@ export type MockCodeSnippet = CodeSnippet & {
 const [list, setList] = useLocalStorageState<MockScriptListItem[]>(
   "mockScriptList",
   {
-    defaultValue: [],
+    defaultValue: [
+      {
+        id: nanoid(),
+        name: "layout_test 超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超长的标题",
+        savePath: "内部存储",
+        version: "v12.2.0",
+        description: "这是一个演示脚本",
+        setting: {
+          autoImportLastRunConfig: true,
+          targetAdbDevice: "",
+          targetApp: "",
+          autoStartTargetApp: false,
+          excludeDevice: [],
+        },
+        content: PLAY_MOCK_SCRIPT,
+      },
+    ],
   }
 );
 const [codeList, setCodeList] = useLocalStorageState<MockCodeSnippet[]>(
   "mockCodeSnippetList",
   {
-    defaultValue: [],
+    defaultValue: [
+      {
+        id: nanoid(),
+        name: "logtype",
+        filePath: "内部存储",
+        description: "编辑器输入logtype以插入本片段",
+        prefix: "logtype",
+        content: `["loading", "success", "danger", "warning", "info"]`,
+      },
+    ],
   }
 );
 const mockScriptList = ref<MockScriptListItem[]>(
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -43,6 +43,302 @@ const main = async () => {
 `;
 };
 
+export const PLAY_MOCK_SCRIPT = `//引用类型推断文件，以便在vscode中获得内置api的类型推断
+//@ts-ignore
+/// <reference path="./lib/csfr.d.ts" />
+/*
+ * 请勿删除，此声明会在脚本读取时用到！
+ * @version:v12.2.0
+ * @name: layout_test 超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超级超长的标题
+ * @description:对脚本交互表单的渲染效果进行展示
+ */
+const { buildForm, log, sleep } = Preludes;
+//脚本运行入口函数
+const main = async () => {
+  //构建默认表单项并渲染表单
+  buildForm([
+    {
+      type: FieldType.Select,
+      label: "运行模式",
+      targetGroupLabel: "选择组件",
+      options: ["运行模式1", "运行模式2", "运行模式3"],
+      value: "运行模式1", //默认值
+    },
+    {
+      id: "__test__",
+      type: FieldType.Select,
+      label: "测试",
+      targetGroupLabel: "选择组件",
+      multiple: true,
+      options: [1, 2, 3, 4],
+      value: [1, 3],
+    },
+    {
+      type: FieldType.Select,
+      label: "最大分数百分比",
+      targetGroupLabel: "选择组件",
+      options: [100, 90, 80],
+      value: 100,
+    },
+    {
+      id: "daojwdoj",
+      type: FieldType.Select,
+      label: "随机最小分数百分比",
+      targetGroupLabel: "选择组件",
+      group: true,
+      multiple: true,
+      options: [
+        {
+          groupLabel: "高分",
+          options: [
+            {
+              value: 90,
+              label: "90分",
+            },
+            {
+              value: 80,
+              label: "80分",
+            },
+            {
+              value: 70,
+              label: "70分",
+            },
+          ],
+        },
+        {
+          groupLabel: "低分",
+          options: [
+            {
+              value: 60,
+              label: "60分",
+            },
+            {
+              value: 50,
+              label: "50分",
+            },
+            {
+              value: 0,
+              label: "0分",
+            },
+          ],
+        },
+      ],
+      value: [90, 80],
+    },
+    {
+      id: "",
+      type: FieldType.Check,
+      label: "随机点击",
+      targetGroupLabel: "复选框组件",
+      checked: false,
+    },
+    {
+      type: FieldType.Check,
+      label: "随机偏移",
+      targetGroupLabel: "复选框组件",
+      checked: true,
+    },
+    {
+      type: FieldType.Input,
+      inputType: "text",
+      label: "密码",
+      targetGroupLabel: "密码组件",
+      value: "-1",
+      mod: "password",
+      clearable: true,
+      showPassword: true,
+      placeholder: "dawkodkwoakdok",
+    },
+    {
+      type: FieldType.Input,
+      label: "密码2",
+      targetGroupLabel: "密码组件",
+      value: "-1",
+      mod: "password",
+      clearable: true,
+      maxlength: 5,
+      showWordLimit: true,
+    },
+    {
+      type: FieldType.Input,
+      label: "请输入随机值的范围",
+      id: "test_2",
+      inputType: "range",
+      targetGroupLabel: "范围输入组件",
+      controls: false,
+      value: [0, 5],
+      limit: [0, 10],
+    },
+    {
+      type: FieldType.Input,
+      label: "说明",
+      targetGroupLabel: "文本域组件",
+      value: "巴拉巴拉~",
+      maxlength: 50,
+      showWordLimit: true,
+      clearable: true,
+      mod: "textarea",
+      autosize: [undefined, 10],
+    },
+    {
+      type: FieldType.Input,
+      inputType: "number",
+      label: "数字1",
+      targetGroupLabel: "数字输入框",
+      value: 123,
+      step: 2,
+      controlsPosition: "right",
+      valueOnClear: "min",
+      min: -5,
+    },
+    {
+      type: FieldType.Input,
+      inputType: "number",
+      label: "数字2",
+      targetGroupLabel: "数字输入框",
+      value: 123,
+      step: 3,
+      max: 129,
+      valueOnClear: "max",
+    },
+    {
+      type: FieldType.Input,
+      label: "简介",
+      targetGroupLabel: "文本输入框",
+      value: "-1",
+      showWordLimit: true,
+      maxlength: 100,
+    },
+    {
+      type: FieldType.Input,
+      label: "子文件(多选)",
+      targetGroupLabel: "文件输入",
+      inputType: "file",
+      multiple: true,
+      value: [],
+    },
+    {
+      type: FieldType.Input,
+      label: "主文件",
+      targetGroupLabel: "文件输入",
+      inputType: "file",
+      multiple: false,
+      value: "",
+    },
+    {
+      id: "daokok",
+      type: FieldType.Picker,
+      label: "颜色",
+      targetGroupLabel: "pickers",
+      pickerType: "color",
+      value: "",
+      colorFormat: "rgb",
+      enableAlpha: false,
+      predefine: [
+        "#ff4500",
+        "#ff8c00",
+        "#ffd700",
+        "#90ee90",
+        "#00ced1",
+        "#1e90ff",
+        "#c71585",
+        "rgba(255, 69, 0, 0.68)",
+        "rgb(255, 120, 0)",
+        "hsv(51, 100, 98)",
+        "hsva(120, 40, 94, 0.5)",
+        "hsl(181, 100%, 37%)",
+        "hsla(209, 100%, 56%, 0.73)",
+        "#c7158577",
+      ],
+    },
+    {
+      type: FieldType.Picker,
+      label: "222",
+      targetGroupLabel: "pickers",
+      value: [],
+      pickerType: "time",
+      isRange: true,
+      valueFormat: "HH:mm:ss",
+      rangeSeparator: "至",
+      startPlaceholder: "起始时间",
+      endPlaceholder: "结束时间",
+    },
+    {
+      type: FieldType.Picker,
+      label: "111",
+      targetGroupLabel: "pickers",
+      value: "",
+      pickerType: "time",
+      isRange: false,
+      valueFormat: "HH/mm/ss",
+      disabledHours: () => [1, 2, 3, 16],
+    },
+    {
+      id: "TM",
+      type: FieldType.Picker,
+      label: "333",
+      targetGroupLabel: "pickers",
+      value: [],
+      pickerType: "date",
+      isRange: true,
+      valueFormat: "YYYY-MM-DD HH:mm:ss",
+    },
+    {
+      type: FieldType.Picker,
+      label: "444",
+      targetGroupLabel: "pickers",
+      value: new Date(),
+      pickerType: "date",
+    },
+    {
+      type: FieldType.Select,
+      label: "部分",
+      targetGroupLabel: "分段选择",
+      segmented: true,
+      options: [
+        {
+          label: "一",
+          value: 1,
+        },
+        {
+          label: "二",
+          value: 2,
+        },
+        {
+          label: "三",
+          value: 3,
+        },
+      ],
+      value: 1,
+    },
+  ]);
+  //等待用户点击"开始"按钮，点击按钮时等待结束，后续可以通过getCustomizeForm获取表单数据
+  const { getFieldValue, getFieldValueById } = await getCustomizeForm();
+  //通过组件类型、标签名、组标签定位到表单项并获取值
+  const mod = getFieldValue(
+    FieldType.Select,
+    "运行模式",
+    "无效的运行模式",
+    "选择组件"
+  );
+  //通过id获取表单项的值
+  const testValue = getFieldValueById<number[]>("__test__", []);
+  //其它表单项的获取方式同上
+  log("运行模式为：" + mod, "loading");
+  log("testValue:" + testValue, "loading");
+  const unlisten = await GlobalShortcut.listen(["Shift+C"], (e) => {
+    log("按键按下" + e.shortcut, "loading");
+    unlisten && unlisten();
+  });
+  const types = ["loading", "success", "danger", "warning", "info"];
+  setInterval(() => {
+    let socre = Math.floor((Math.random() * 100 - 1) / 20);
+    log(Math.random(), types[socre] as any);
+  }, 500);
+  await sleep(1000 * 3000);
+};
+`;
+
 export const IS_PLAYGROUND_ENV = import.meta.env.MODE === "play";
 
 //code snippets
