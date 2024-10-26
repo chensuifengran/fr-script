3b86e0570eb372e63dce7b0e466d9d27e0f3b937
尘随风染 <2529720581@qq.com>
2024-10-26 17:57:02 +0800

    refactor: define string enumeration constants as objects

--- src/invokes/enums.ts
+++ src/invokes/enums.ts
@@ -1,37 +1,66 @@
-export const ENUM_DECLARE = `
-declare enum FieldType {
-  /**
-   * @description BuildForm中，type为"check"的表单项
-   */
-  Check = "check",
-  /**
-   * @description BuildForm中，type为"input"的表单项
-   */
-  Input = "input",
-  /**
-   * @description BuildForm中，type为"select"的表单项
-   */
-  Select = "select",
-  /**
-   * @description BuildForm中，type为"picker"的表单项
-   */
-  Picker = "picker",
-  /**
-   * @deprecated 请使用FieldType.Check代替,将在未来版本中移除
-   */
-  CheckList = "checkList",
-  /**
-   * @deprecated 请使用FieldType.Input代替,将在未来版本中移除
-   */
-  InputList = "inputList",
-  /**
-   * @deprecated 请使用FieldType.Select代替,将在未来版本中移除
-   */
-  SelectList = "selectList",
-  /**
-   * @deprecated 请使用FieldType.Picker代替,将在未来版本中移除
-   */
-  PickerList = "pickerList",
-}
-`;
-export const ENUM_CODE = ENUM_DECLARE.replace(/declare enum/g, "enum");
+export const inject_enums: Record<
+  /*枚举类型命名*/ string,
+  Record<
+    /*枚举值名称*/ string,
+    {
+      description?: string; //枚举值描述
+      value: string; //枚举值
+      deprecated?: string; //是否已废弃
+    }
+  >
+> = {
+  FieldType: {
+    Check: {
+      description: 'BuildForm中，type为"check"的表单项',
+      value: "check",
+    },
+    Input: {
+      description: 'BuildForm中，type为"input"的表单项',
+      value: "input",
+    },
+    Select: {
+      description: 'BuildForm中，type为"select"的表单项',
+      value: "select",
+    },
+    Picker: {
+      description: 'BuildForm中，type为"picker"的表单项',
+      value: "picker",
+    },
+    CheckList: {
+      deprecated: "请使用FieldType.Check代替,将在未来版本中移除",
+      value: "checkList",
+    },
+    InputList: {
+      deprecated: "请使用FieldType.Input代替,将在未来版本中移除",
+      value: "inputList",
+    },
+    SelectList: {
+      deprecated: "请使用FieldType.Select代替,将在未来版本中移除",
+      value: "selectList",
+    },
+    PickerList: {
+      deprecated: "请使用FieldType.Picker代替,将在未来版本中移除",
+      value: "pickerList",
+    },
+  },
+};
+
+const genEnumDeclare = (removeDecalreWord?: boolean) => {
+  let declare = (!removeDecalreWord ? "declare " : "") + `enum FieldType {\n`;
+  for (const key in inject_enums.FieldType) {
+    const item = inject_enums.FieldType[key];
+    declare += `  /**\n`;
+    if (item.description) {
+      declare += `   * @description ${item.description}\n`;
+    }
+    if (item.deprecated) {
+      declare += `   * @deprecated ${item.deprecated}\n`;
+    }
+    declare += `   */\n  ${key} = "${item.value}",\n`;
+  }
+  declare += `}\n`;
+  return declare;
+};
+
+export const ENUM_DECLARE = genEnumDeclare();
+export const ENUM_CODE = genEnumDeclare(true);
