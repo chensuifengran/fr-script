319fb00f8c81fa9459799ac17296e03fdccf35bb
尘随风染 <2529720581@qq.com>
2024-10-27 20:33:30 +0800

    chore:  optimize the vite plugin and change js script to ts module

--- vitePlugins/hotUpdate.ts
+++ vitePlugins/hotUpdate.ts
@@ -1,87 +1,22 @@
 import { Plugin } from "vite";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-import { exec } from "child_process";
-import chalk from "chalk";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const scriptPath = resolve(__dirname, "../script/genBuiltInApiType.js");
-let timer: any;
-const runScript = (type: "api" | "declare") => {
-  timer && clearTimeout(timer);
-  timer = setTimeout(() => {
-    //使用node执行scriptPath脚本
-    exec(`node ${scriptPath} ${type}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`执行错误: ${error}`);
-        return;
-      }
-      //HH:mm:ss
-      const date = new Date().toLocaleTimeString().slice(0, 8);
-      if (stderr) {
-        console.error(
-          date,
-          chalk.blue("[hot-update-plugin]"),
-          chalk.red(stderr)
-        );
-      } else {
-        console.log(
-          date,
-          chalk.blue("[hot-update-plugin]"),
-          chalk.green(stdout)
-        );
-      }
-    });
-  }, 500);
-};
-const genDeclareScriptPath = resolve(__dirname, "../script/genApiDeclare.js");
-let genApiDeclareTimer: any;
-const getDeclareStr = (target: "buildForm" | "rendererList") => {
-  genApiDeclareTimer && clearTimeout(genApiDeclareTimer);
-  genApiDeclareTimer = setTimeout(() => {
-    exec(`node ${genDeclareScriptPath} ${target}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`执行错误: ${error}`);
-        return;
-      }
-      //HH:mm:ss
-      const date = new Date().toLocaleTimeString().slice(0, 8);
-      if (stderr) {
-        console.error(
-          date,
-          chalk.blue("[hot-update-plugin]"),
-          chalk.red(stderr)
-        );
-      } else {
-        console.log(
-          date,
-          chalk.blue("[hot-update-plugin]"),
-          chalk.green(stdout)
-        );
-      }
-    });
-  }, 500);
-};
-const getEnumsScriptPath = resolve(__dirname, "../script/genEnumDeclare.js");
-const runGenEnums = () => {
-  exec(`node ${getEnumsScriptPath}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行错误: ${error}`);
-      return;
-    }
-    //HH:mm:ss
-    const date = new Date().toLocaleTimeString().slice(0, 8);
-    if (stderr) {
-      console.error(date, chalk.blue("[hot-update-plugin]"), chalk.red(stderr));
-    } else {
-      console.log(date, chalk.blue("[hot-update-plugin]"), chalk.green(stdout));
-    }
-  });
-};
+import {
+  genEnumDTS,
+  genBuiltInApiDTS,
+  genBuildFormDTS,
+  genAllDTS,
+} from "./genDTS";
+import { changeLogLevel } from "./logConf";
 export function hotUpdatePlugin(): Plugin {
   return {
     name: "hot-update-plugin",
+    configResolved(config) {
+      const mode = config.env.MODE;
+      if (mode && mode.includes(":")) {
+        const [_, logLevel] = mode.split(":");
+        changeLogLevel(logLevel);
+      }
+      genAllDTS();
+    },
     handleHotUpdate({ file }) {
       if (file.endsWith(".ts")) {
         const excludeFiles = [
@@ -89,23 +24,23 @@ export function hotUpdatePlugin(): Plugin {
           "core.d.ts",
           "buildFormDeclare.ts",
           "rendererListDeclare.ts",
-          "enums.ts",
+          "enums.d.ts",
         ];
         if (excludeFiles.find((e) => file.includes(e))) {
           return;
         }
         if (file.includes("invokes/")) {
-          runScript("api");
+          genBuiltInApiDTS();
         }
         if (file.includes("useScriptApi.ts") || file.includes("useCore.ts")) {
-          runScript("declare");
+          genBuiltInApiDTS("declare");
         }
         if (file.includes("buildForm.d.ts")) {
-          getDeclareStr("buildForm");
+          genBuildFormDTS("buildForm");
         } else if (file.includes("rendererList.d.ts")) {
-          getDeclareStr("rendererList");
-        } else if (file.includes("enums.d.ts")) {
-          runGenEnums();
+          genBuildFormDTS("rendererList");
+        } else if (file.includes("enums.ts")) {
+          genEnumDTS();
         }
       }
     },
---
+++ vitePlugins/logConf.ts
@@ -0,0 +1,59 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import { writeFileSync, existsSync, readFileSync } from "fs";
+import chalk from "chalk";
+const LOG_4_RS_YML = `
+refresh_rate: 30 seconds
+appenders:
+  stdout:
+    kind: console
+  rolling_file:
+    kind: rolling_file
+    path: "logs/application-errors.log"
+    policy:
+      kind: compound
+      trigger:
+        kind: size
+        limit: 10000000
+      roller:
+        kind: delete
+    encoder:
+      pattern: "{d(%Y-%m-%d %H:%M:%S%.3f)} {M}: {m}{n}"
+root:
+  level: __LOG_LEVEL__
+  appenders:
+    - stdout
+    - rolling_file
+`;
+export const changeLogLevel = (level: string = "DEBUG") => {
+  if (
+    level !== "DEBUG" &&
+    level !== "INFO" &&
+    level !== "WARN" &&
+    level !== "ERROR"
+  ) {
+    console.log("❌ 日志级别设置错误，请检查输入是否正确");
+    return;
+  }
+  const logYmlPath = resolve(__dirname, "../src-tauri/resources/log4rs.yml");
+  const log4rsYml = LOG_4_RS_YML.replace("__LOG_LEVEL__", level);
+  let needUpdate = true;
+  if (existsSync(logYmlPath)) {
+    const yml = readFileSync(logYmlPath, "utf-8");
+    if (yml.trim() === log4rsYml.trim()) {
+      needUpdate = false;
+    }
+  }
+  if (needUpdate) {
+    writeFileSync(logYmlPath, log4rsYml, {
+      encoding: "utf-8",
+    });
+    console.log("✨ 日志级别已设置为：", chalk.yellow(level));
+  }
+  console.log(
+    "当前环境：",
+    chalk.green(level === "DEBUG" ? "development" : "production")
+  );
+};
