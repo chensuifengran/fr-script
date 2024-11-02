2f67e0f184ecbfee713e8fc88f412a40a16a956e
尘随风染 <2529720581@qq.com>
2024-11-02 18:58:37 +0800

    refactor: support converting dateString to new Date expression

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,15 +1,32 @@
+const DATE_PREFIX = "__D2S__";
+export const processDate = (strOrDate: string | Date) => {
+  if (strOrDate instanceof Date) {
+    return `${DATE_PREFIX}${strOrDate.toLocaleDateString()}`;
+  }
+  return strOrDate;
+};
+
 export const objectToString = (
   target: any,
   indent: number = 2,
-  currentIndent: number = 0
+  currentIndent: number = 0,
+  resetDateString: boolean = false
 ): string => {
   const space = " ".repeat(currentIndent + indent);
   const closingSpace = " ".repeat(currentIndent);
   if (Array.isArray(target)) {
     const arrayValues = target.map((item) => {
       if (typeof item === "object" && item !== null) {
-        return objectToString(item, indent, currentIndent + indent);
+        return objectToString(
+          item,
+          indent,
+          currentIndent + indent,
+          resetDateString
+        );
       } else if (typeof item === "string") {
+        if (resetDateString && item.startsWith(DATE_PREFIX)) {
+          return `new Date("${item.replace(DATE_PREFIX, "")}")`;
+        }
         return `"${item}"`;
       } else if (typeof item === "function") {
         return item.toString();
@@ -24,8 +41,16 @@ export const objectToString = (
     if (Array.isArray(value)) {
       const arrayValues = value.map((item) => {
         if (typeof item === "object" && item !== null) {
-          return objectToString(item, indent, currentIndent + indent);
+          return objectToString(
+            item,
+            indent,
+            currentIndent + indent,
+            resetDateString
+          );
         } else if (typeof item === "string") {
+          if (resetDateString && item.startsWith(DATE_PREFIX)) {
+            return `new Date("${item.replace(DATE_PREFIX, "")}")`;
+          }
           return `"${item}"`;
         } else if (typeof item === "function") {
           return item.toString();
@@ -37,8 +62,16 @@ export const objectToString = (
         `,\n${space}${space}`
       )}\n${space}${closingSpace}]`;
     } else if (typeof value === "object" && value !== null) {
-      return `${key}: ${objectToString(value, indent, currentIndent + indent)}`;
+      return `${key}: ${objectToString(
+        value,
+        indent,
+        currentIndent + indent,
+        resetDateString
+      )}`;
     } else if (typeof value === "string") {
+      if (resetDateString && value.startsWith(DATE_PREFIX)) {
+        return `${key}: new Date("${value.replace(DATE_PREFIX, "")}")`;
+      }
       return `${key}: "${value}"`;
     } else if (typeof value === "function") {
       return `${key}: ${value.toString()}`;
