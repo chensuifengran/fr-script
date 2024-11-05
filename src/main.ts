21facb0372682f9863d65e35c30465d4622a3e72
尘随风染 <2529720581@qq.com>
2024-11-05 19:36:31 +0800

    feat: import vue3-context-menu.css

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import "@imengyu/vue3-context-menu/lib/vue3-context-menu.css";
 import App from "./App.vue";
-import './assets/scss/theme.scss'
+import "./assets/scss/theme.scss";
 import "./assets/scss/style.scss";
 import "./assets/scss/highlight-vs.scss";
 import router from "./router/index";
-import 'animate.css'
-import 'element-plus/theme-chalk/el-message-box.css'//手动引入el-message-box样式，解决el-message-box样式丢失问题
-import 'virtual:uno.css'
+import "animate.css";
+import "element-plus/theme-chalk/el-message-box.css"; //手动引入el-message-box样式，解决el-message-box样式丢失问题
+import "virtual:uno.css";
 
 const pinia = createPinia();
 const app = createApp(App);
