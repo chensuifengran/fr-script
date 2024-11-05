5a36904bdaed387336aa218f2257d5347933e456
尘随风染 <2529720581@qq.com>
2024-11-05 19:33:55 +0800

    feat: add icons

---
+++ src/components/Icons/PuzzleEditIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-icon size="large"><span i-mdi-puzzle-edit-outline></span></el-icon>
+</template>
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Icons/PuzzleRemoveIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-icon size="large" color="var(--el-color-danger)"><span i-mdi-puzzle-remove-outline></span></el-icon>
+</template>
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
