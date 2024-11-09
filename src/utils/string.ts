bdc1abb462947f0714de309ad7399c9fe2ce677d
尘随风染 <2529720581@qq.com>
2024-11-09 15:54:58 +0800

    fix: date processing retention time and keys that ignore undefined values

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,7 @@
 const DATE_PREFIX = "__D2S__";
 export const processDate = (strOrDate: string | Date) => {
   if (strOrDate instanceof Date) {
-    return `${DATE_PREFIX}${strOrDate.toLocaleDateString()}`;
+    return `${DATE_PREFIX}${strOrDate.toLocaleString()}`;
   }
   return strOrDate;
 };
@@ -25,7 +25,7 @@ export const objectToString = (
         );
       } else if (typeof item === "string") {
         if (resetDateString && item.startsWith(DATE_PREFIX)) {
-          return `new Date("${item.replace(DATE_PREFIX, "")}")`;
+          return `new Date(${item.replace(DATE_PREFIX, "")})`;
         }
         return `"${item}"`;
       } else if (typeof item === "function") {
@@ -37,48 +37,53 @@ export const objectToString = (
     return `[\n${space}${arrayValues.join(`,\n${space}`)}\n${closingSpace}]`;
   }
 
-  const entries = Object.entries(target).map(([key, value]) => {
-    if (Array.isArray(value)) {
-      const arrayValues = value.map((item) => {
-        if (typeof item === "object" && item !== null) {
-          return objectToString(
-            item,
-            indent,
-            currentIndent + indent,
-            resetDateString
-          );
-        } else if (typeof item === "string") {
-          if (resetDateString && item.startsWith(DATE_PREFIX)) {
-            return `new Date("${item.replace(DATE_PREFIX, "")}")`;
-          }
-          return `"${item}"`;
-        } else if (typeof item === "function") {
-          return item.toString();
-        } else {
-          return item;
-        }
-      });
-      return `${key}: [\n${space}${space}${arrayValues.join(
-        `,\n${space}${space}`
-      )}\n${space}${closingSpace}]`;
-    } else if (typeof value === "object" && value !== null) {
-      return `${key}: ${objectToString(
-        value,
-        indent,
-        currentIndent + indent,
-        resetDateString
-      )}`;
-    } else if (typeof value === "string") {
-      if (resetDateString && value.startsWith(DATE_PREFIX)) {
-        return `${key}: new Date("${value.replace(DATE_PREFIX, "")}")`;
+  const entries = Object.entries(target)
+    .map(([key, value]) => {
+      if (value === undefined) {
+        return "";
       }
-      return `${key}: "${value}"`;
-    } else if (typeof value === "function") {
-      return `${key}: ${value.toString()}`;
-    } else {
-      return `${key}: ${value}`;
-    }
-  });
+      if (Array.isArray(value)) {
+        const arrayValues = value.map((item) => {
+          if (typeof item === "object" && item !== null) {
+            return objectToString(
+              item,
+              indent,
+              currentIndent + indent,
+              resetDateString
+            );
+          } else if (typeof item === "string") {
+            if (resetDateString && item.startsWith(DATE_PREFIX)) {
+              return `new Date("${item.replace(DATE_PREFIX, "")}")`;
+            }
+            return `"${item}"`;
+          } else if (typeof item === "function") {
+            return item.toString();
+          } else {
+            return item;
+          }
+        });
+        return `${key}: [\n${space}${space}${arrayValues.join(
+          `,\n${space}${space}`
+        )}\n${space}${closingSpace}]`;
+      } else if (typeof value === "object" && value !== null) {
+        return `${key}: ${objectToString(
+          value,
+          indent,
+          currentIndent + indent,
+          resetDateString
+        )}`;
+      } else if (typeof value === "string") {
+        if (resetDateString && value.startsWith(DATE_PREFIX)) {
+          return `${key}: new Date("${value.replace(DATE_PREFIX, "")}")`;
+        }
+        return `${key}: "${value}"`;
+      } else if (typeof value === "function") {
+        return `${key}: ${value.toString()}`;
+      } else {
+        return `${key}: ${value}`;
+      }
+    })
+    .filter(Boolean);
   const result = `{\n${space}${entries.join(`,\n${space}`)}\n${closingSpace}}`;
   return result;
 };
