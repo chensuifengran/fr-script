47d262b7e39d52f970930ea8a06fd5cb2ae20ccf
尘随风染 <2529720581@qq.com>
2024-11-13 15:09:34 +0800

    feat: add mismatchCallback parameter to waitText and add js-doc comments

--- src/invokes/ocr/declaration.ts
+++ src/invokes/ocr/declaration.ts
@@ -1,10 +1,10 @@
 export const declaration = `
 declare type OCRResult = {
   position: [
-      [number, number],
-      [number, number],
-      [number, number],
-      [number, number]
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number]
   ];
   text: string;
   score: number;
@@ -19,7 +19,14 @@ declare class FindResult {
     score: number,
     offset?: [number, number]
   );
+  /**
+   * 鼠标点击目标元素的中心位置
+   */
   public click(): Promise<void>;
+  /**
+   * [ADB]触摸目标元素的中心位置
+   * @returns 触摸命令执行结果，一般可忽略
+   */
   public touch(): Promise<string>;
 }
 declare class OcrUtil {
@@ -29,11 +36,52 @@ declare class OcrUtil {
     x: number;
     y: number;
   };
-  constructor(originX: number, originY: number, result: OCRResult[], reCall:() => Promise<OcrUtil | undefined>);
+  constructor(
+    originX: number,
+    originY: number,
+    result: OCRResult[],
+    reCall: () => Promise<OcrUtil | undefined>
+  );
+  /**
+   * 判断是否包含指定文本
+   * @param texts 文本数组
+   * @returns 是否包含数组中的任意一个文本
+   */
   public includes(texts: string[]): boolean;
-  public searchText(text: string, offset?: [number, number]) : FindResult[];
-  public findText(text: string, offset?: [number, number]) : FindResult | undefined;
-  public waitText(text: string,adb?: boolean, sleepMs?: number, maxWaitCount?: number) : Promise<boolean>;
+
+  /**
+   * 查找文本，返回所有匹配的结果
+   * @param text 文本
+   * @param offset 文本位置附加的偏移量
+   * @returns 匹配结果数组
+   */
+  public searchText(text: string, offset?: [number, number]): FindResult[];
+  /**
+   * 查找文本，返回第一个匹配的结果
+   * @param text 文本
+   * @param offset 文本位置附加的偏移量
+   * @returns 匹配结果
+   */
+  public findText(
+    text: string,
+    offset?: [number, number]
+  ): FindResult | undefined;
+  /**
+   * 等待文本出现
+   * @param text 文本
+   * @param adb 识别前是否执行adb截图
+   * @param sleepMs 检测间隔，默认1000ms，不含识别时间
+   * @param maxWaitCount 最大检测次数,默认10次
+   * @param mismatchCallback 检测时，文本不匹配时的回调
+   * @returns 文本出现为true，到达最大检测次数时为false
+   */
+  public waitText(
+    text: string,
+    adb?: boolean,
+    sleepMs?: number,
+    maxWaitCount?: number,
+    mismatchCallback?: (curText: OCRResult[], expectText: string) => void
+  ): Promise<boolean>;
 }
 declare function ocr(
   x: number,
@@ -42,4 +90,4 @@ declare function ocr(
   height: number,
   imgPath?: string
 ): Promise<OcrUtil | undefined>;
-`
+`;
--- src/invokes/ocr/document.ts
+++ src/invokes/ocr/document.ts
@@ -50,33 +50,58 @@ declare class OcrUtil {
     y: number;
   };
   constructor(
-    originX: number, 
-    originY: number, 
-    result: OCRResult[], 
-    reCall:() => Promise<OcrUtil | undefined>
+    originX: number,
+    originY: number,
+    result: OCRResult[],
+    reCall: () => Promise<OcrUtil | undefined>
   );
+  /**
+   * 判断是否包含指定文本
+   * @param texts 文本数组
+   * @returns 是否包含数组中的任意一个文本
+   */
   public includes(texts: string[]): boolean;
-  public searchText(
-    text: string, 
-    offset?: [number, number]
-  ) : FindResult[];
+
+  /**
+   * 查找文本，返回所有匹配的结果
+   * @param text 文本
+   * @param offset 文本位置附加的偏移量
+   * @returns 匹配结果数组
+   */
+  public searchText(text: string, offset?: [number, number]): FindResult[];
+  /**
+   * 查找文本，返回第一个匹配的结果
+   * @param text 文本
+   * @param offset 文本位置附加的偏移量
+   * @returns 匹配结果
+   */
   public findText(
-    text: string, 
+    text: string,
     offset?: [number, number]
-  ) : FindResult | undefined;
+  ): FindResult | undefined;
+  /**
+   * 等待文本出现
+   * @param text 文本
+   * @param adb 识别前是否执行adb截图
+   * @param sleepMs 检测间隔，默认1000ms，不含识别时间
+   * @param maxWaitCount 最大检测次数,默认10次
+   * @param mismatchCallback 文本不匹配时的回调,result为识别的结果,expectText为预期文本
+   * @returns 文本出现为true，到达最大检测次数时为false
+   */
   public waitText(
     text: string,
-    adb?: boolean, 
-    sleepMs?: number, 
-    maxWaitCount?: number
+    adb?: boolean,
+    sleepMs?: number,
+    maxWaitCount?: number,
+    mismatchCallback?: (result: OCRResult[], expectText: string) => void
   ): Promise<boolean>;
 }
 declare type OCRResult = {
   position: [
-      [number, number],
-      [number, number],
-      [number, number],
-      [number, number]
+    [number, number],
+    [number, number],
+    [number, number],
+    [number, number]
   ];
   text: string;
   score: number;
@@ -91,7 +116,14 @@ declare class FindResult {
     score: number,
     offset?: [number, number]
   );
+  /**
+   * 鼠标点击目标元素的中心位置
+   */
   public click(): Promise<void>;
+  /**
+   * [ADB]触摸目标元素的中心位置
+   * @returns 触摸命令执行结果，一般可忽略
+   */
   public touch(): Promise<string>;
 }
 `,
